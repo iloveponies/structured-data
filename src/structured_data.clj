8be957ce8bfb7f3bd9e445d28ad231a51b0c9a8a
Antti O P Karjalainen <aopkarja@dhp-26.cs.helsinki.fi>
2012-10-22 16:40:22 +0300

    Third one partially done...

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [z (+ x x)]
+    (Math/pow z z)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x, y, z] v]
+    (+ x z)))
 
 (defn point [x y]
   [x y])
@@ -18,35 +20,46 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
+(defn width [[[x1 y1] [x2 y2]]]
+	(if (> x2 x1)
+      (- x2 x1)
+      (- x1 x2)))
 
-(defn height [rectangle]
-  :-)
+(defn height [[[x1 y1] [x2 y2]]]
+  	(if (> y2 y1)
+      (- y2 y1)
+      (- y1 y2)))
 
 (defn square? [rectangle]
-  :-)
+  (= (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
-(defn contains-point? [rectangle point]
-  :-)
+(defn contains-point? [[[x1 y1] [x2 y2]][x y]]
+  (if (and (<= x1 x x2) (<= y1 y y2))
+    true
+    false))
 
-(defn contains-rectangle? [outer inner]
-  :-)
+(defn contains-rectangle? [outer [point1 point2]]
+  (and (contains-point? outer point1) (contains-point? outer point2)))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
   :-)
 
 (defn multiple-authors? [book]
-  :-)
+  (let [count (author-count book)]
+    (cond
+     (= count 0) false
+     (= count 1) false
+     :else true
+     )))
 
 (defn add-author [book new-author]
-  :-)
+  (conj (:authors book) new-author)) ;TODO
 
 (defn alive? [author]
   :-)
@@ -110,5 +123,3 @@
 
 (defn books-by-living-authors [books]
   :-)
-
-; %________%
