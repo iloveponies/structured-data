a5b110563db3c7e8e60ebf355554ea9cb3a10c1e
Lassi J Vapaakallio <lvapaaka@dhp-2.cs.helsinki.fi>
2012-10-24 13:25:05 +0300

    WOO

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+	(let [xx (+ x x)]
+		(Math/pow xx xx)))
 
 (defn spiff [v]
-  :-)
+	(+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z] v]
+	(+ x z)))
 
 (defn point [x y]
   [x y])
@@ -19,96 +21,129 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1,y1] [x2,y2]] rectangle]
+		(if (< x1 x2) (- x2 x1) (- x1 x2))))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1,y1] [x2,y2]] rectangle]
+		(if (< y1 y2) (- y2 y1) (- y1 y2))))
 
 (defn square? [rectangle]
-  :-)
+  (== (height rectangle) (width rectangle))
+  )
 
 (defn area [rectangle]
-  :-)
+	(* (height rectangle) (width rectangle))
+  )
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1,y1] [x2,y2]] rectangle]
+		(let [[findx,findy] point]
+          (boolean (and (<= x1 findx x2) 
+               			(<= y1 findy y2))))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[p1 p2] inner]
+    (and (contains-point? outer p1)
+		 (contains-point? outer p2))))
 
 (defn title-length [book]
-  :-)
+  (let [title (:title book)]
+    (count title)))
 
 (defn author-count [book]
-  :-)
+  (count (get book :authors)))
 
 (defn multiple-authors? [book]
-  :-)
+  (< 1 (author-count book)))
 
 (defn add-author [book new-author]
-  :-)
+	(let [old-authors (get book :authors)]
+		(assoc book :authors (conj old-authors new-author))))
 
 (defn alive? [author]
-  :-)
+	(not (contains? author :death-year)))
 
 (defn element-lengths [collection]
-  :-)
+  (map count collection))
 
 (defn second-elements [collection]
-  :-)
+  (let [second (fn [v] (get v 1))]
+	(map second collection)))
 
 (defn titles [books]
-  :-)
+  (map :title books))
 
 (defn monotonic? [a-seq]
-  :-)
+  (or (apply <= a-seq) (apply >= a-seq)))
 
 (defn stars [n]
-  :-)
+  (apply str (repeat n "*")))
 
 (defn toggle [a-set elem]
-  :-)
+  (if (contains? a-set elem) (disj a-set elem) (conj a-set elem)))
 
 (defn contains-duplicates? [a-seq]
-  :-)
+  (not (== (count a-seq) (count (set a-seq))))) 
 
 (defn old-book->new-book [book]
-  :-)
+	(let [old-authors (get book :authors)]
+  		(assoc book :authors (set old-authors))))
 
 (defn has-author? [book author]
-  :-)
+  (let [bauthor (get book :authors)]
+    (contains? bauthor author)))
 
 (defn authors [books]
-  :-)
+  (let [get-authors
+        (fn [book] (:authors book))]
+    (apply clojure.set/union (map get-authors books))))
 
 (defn all-author-names [books]
-  :-)
+  (let [writers (authors books)]
+	(set (map :name writers))))
 
 (defn author->string [author]
-  :-)
+  (let [nam (:name author)
+    byear (:birth-year author)
+    dyear (:death-year author)]
+    (str nam (if dyear 
+               (str " (" byear " - " dyear ")")
+               (if byear 
+                 (str " (" byear " - " ")"))))))  
 
 (defn authors->string [authors]
-  :-)
+  (apply str (interpose ", " (map author->string authors))))
 
 (defn book->string [book]
-  :-)
+  (let [title (:title book)
+        writers (:authors book)]
+    (str title ", written by " (authors->string writers))))
 
 (defn books->string [books]
-  :-)
+	(let [amount (count books)]
+      (str (if (== amount 0) "No books"
+  		(apply str (count books) " book"
+           (if (> amount 1) "s")
+       			". " (interpose ". " (map book->string books))))
+        (str "."))))
 
 (defn books-by-author [author books]
-  :-)
+  (filter (fn [book] (has-author? book author)) books))
 
 (defn author-by-name [name authors]
-  :-)
+  (if (= (:name (first authors)) name) 
+    	(first authors)
+    	(if (> (count authors) 0)
+            (author-by-name name (rest authors))
+          	nil)))
 
 (defn living-authors [authors]
-  :-)
+  (filter alive? authors))
 
 (defn has-a-living-author? [book]
-  :-)
+  (let [authors (:authors book)]
+    (< 0 (count (living-authors authors)))))
 
 (defn books-by-living-authors [books]
-  :-)
-
-; %________%
+  (filter has-a-living-author? books))
--- test/structured_data_test.clj
+++ test/structured_data_test.clj
@@ -133,14 +133,18 @@
   
   (facts "titles"
     (titles [cities]) => ["The City and the City"]
-    (titles books)    => ["The City and the City" "Wild Seed" "Embassytown"]))
+    (titles books)    => (just ["The City and the City"
+                                "Wild Seed"
+                                "Embassytown"
+                                "The Little Schemer"]
+                               :in-any-order)))
 
 (facts "monotonic?"
-  (monotonic? [1 2 3])     => true
-  (monotonic? [0 1 10 11]) => true
-  (monotonic? [3 2 0 -3])  => true
-  (monotonic? [3 2 2])     => true
-  (monotonic? [1 2 1 0])   => false)
+       (monotonic? [1 2 3])     => true
+       (monotonic? [0 1 10 11]) => true
+       (monotonic? [3 2 0 -3])  => true
+       (monotonic? [3 2 2])     => true
+       (monotonic? [1 2 1 0])   => false)
 
 (facts "stars"
   (stars 1) => "*"
@@ -176,7 +180,7 @@
       dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982}
       zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995}
 
-      authors #{china, felleisen, octavia, friedman}
+      authors-set #{china, felleisen, octavia, friedman}
 
       cities {:title "The City and the City" :authors #{china}}
       wild-seed {:title "Wild Seed", :authors #{octavia}}
@@ -208,7 +212,6 @@
     (all-author-names books)
       => #{"Matthias Felleisen" "China Miéville"
            "Octavia E. Butler" "Daniel Friedman"})
-
   (facts "author->string"
     (author->string felleisen) => "Matthias Felleisen"
     (author->string friedman)  => "Daniel Friedman (1944 - )"
@@ -227,7 +230,8 @@
                           (contains ", ")))
 
   (facts "book->string"
-    (book->string wild-seed) => "Wild Seed, written by Octavia E. Butler"
+         (book->string wild-seed)
+         => "Wild Seed, written by Octavia E. Butler (1947 - 2006)"
     (book->string little-schemer)
       => (every-checker (has-prefix "The Little Schemer, written by ")
                         (has-suffix #"Daniel Friedman \(1944 - \), Matthias Felleisen|Matthias Felleisen, Daniel Friedman \(1944 - \)")))
@@ -244,13 +248,13 @@
     (books-by-author octavia books) => (just [wild-seed]))
 
   (facts "author-by-name"
-    (author-by-name "Octavia E. Butler" authors)                => octavia
+    (author-by-name "Octavia E. Butler" authors-set)            => octavia
     (author-by-name "Octavia E. Butler" #{felleisen, friedman}) => nil
-    (author-by-name "China Miéville" authors)                   => china
-    (author-by-name "Goerge R. R. Martin" authors)              => nil)
+    (author-by-name "China Miéville" authors-set)               => china
+    (author-by-name "Goerge R. R. Martin" authors-set)          => nil)
 
   (facts "living-authors"
-    (living-authors authors)             => (just #{china, felleisen, friedman})
+    (living-authors authors-set)         => (just #{china, felleisen, friedman})
     (living-authors #{octavia})          => (just #{})
     (living-authors #{china, felleisen}) => (just #{china, felleisen}))
 
@@ -264,6 +268,4 @@
   (facts "books-by-living-authors"
     (books-by-living-authors books) => (just #{little-schemer cities embassytown})
     (books-by-living-authors (concat books [deus-irae, silmarillion]))
-      => (just #{little-schemer cities embassytown silmarillion})))
-
-; %____%
+    => (just #{little-schemer cities embassytown silmarillion})))
