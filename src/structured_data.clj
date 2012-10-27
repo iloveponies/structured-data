2cfccfb7a433771800c2d07049fb7f0f0779d5b3
Anttipekka Mölsä <amolsa@cs.helsinki.fi>
2012-10-27 16:15:45 +0300

    osa1

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,22 @@
 (ns structured-data)
 
-(defn do-a-thing [x]
-  :-)
+(defn do-a-thing [x] 
+  (let [k (+ x x)] 
+	(Math/pow k k ))
+  )
 
 (defn spiff [v]
-  :-)
+	(+ (get v 0) (get v 2))
+  )
 
 (defn cutify [v]
-  :-)
+	(conj v "<3")   
+  )
 
 (defn spiff-destructuring [v]
-  :-)
+	(let [[x y z] v]
+  (+ x z )
+  ))
 
 (defn point [x y]
   [x y])
@@ -19,16 +25,28 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+	(let [[[x1 y1] [x2 y2]] rectangle]
+		(- x2 x1)
+      )
+  )
 
 (defn height [rectangle]
-  :-)
+	(let [[[x1 y1] [x2 y2]] rectangle]
+		(- x2 x1) (- y2 y1)
+      )
+  )
 
 (defn square? [rectangle]
-  :-)
+	(let [[[x1 y1] [x2 y2]] rectangle]
+		(if(= (- x1 x2) (- y1 y2) ) true false
+      ))
+  )
 
 (defn area [rectangle]
-  :-)
+	(let [[[x1 y1] [x2 y2]] rectangle]
+		(* (height rectangle) (width rectangle))
+      )
+  )
 
 (defn contains-point? [rectangle point]
   :-)
@@ -37,22 +55,30 @@
   :-)
 
 (defn title-length [book]
-  :-)
+	(count(:title book))
+  )
 
 (defn author-count [book]
-  :-)
+   (count(:authors book))
+  )
 
 (defn multiple-authors? [book]
-  :-)
+	(if (> (author-count book) 1) true false)
+  )
 
 (defn add-author [book new-author]
-  :-)
+  (let [authors (get book :authors)]
+    (assoc book :authors (conj authors new-author))
+    )
+  )
 
 (defn alive? [author]
-  :-)
+	(if (get author :death-year) false true)
+  )
 
 (defn element-lengths [collection]
-  :-)
+  (let [laske (fn [x] (count x))]
+    (map laske collection)))
 
 (defn second-elements [collection]
   :-)
@@ -110,5 +136,3 @@
 
 (defn books-by-living-authors [books]
   :-)
-
-; %________%
