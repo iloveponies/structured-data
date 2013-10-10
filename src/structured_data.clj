6c3c49efdcc4f371e9d2de1de7c088fad33fc3a7
Niko O Ryytty <nryytty@z1-1.cs.helsinki.fi>
2013-10-10 13:51:24 +0300

    white pony

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,17 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+  (Math/pow xx xx)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
-(defn spiff-destructuring [v]
-  :-)
+(defn spiff-destructuring [[x0 x1 x2]]
+    (+ x0 x2))
 
 (defn point [x y]
   [x y])
@@ -18,41 +19,42 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
+(defn height [[[x1 y1] [x2 y2]]]
+  (- y2 y1))
 
-(defn height [rectangle]
-  :-)
+(defn width [[[x1 y1] [x2 y2]]]
+  (- x2 x1))
 
 (defn square? [rectangle]
-  :-)
+  (= (height rectangle) (width rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
 
-(defn contains-point? [rectangle point]
-  :-)
+(defn contains-point? [[[x1 y1] [x2 y2]] [x y]]
+  (and (<= x1 x x2) (<= y1 y y2)))
 
-(defn contains-rectangle? [outer inner]
-  :-)
+(defn contains-rectangle? [[[outer-x1 outer-y1] [outer-x2 outer-y2]] [[x1 y1] [x2 y2]]]
+  (and (<= outer-x1 x1 outer-x2) (<= outer-x1 x2 outer-x2) (<= outer-y1 y1 outer-y2) (<= outer-y1 y2 outer-y2)))
 
 (defn title-length [book]
-  :-)
+  (count (get book :title)))
 
 (defn author-count [book]
-  :-)
+  (count (get book :authors)))
 
 (defn multiple-authors? [book]
-  :-)
+  (< 1 (author-count book)))
 
 (defn add-author [book new-author]
-  :-)
+  (let [new-authors (conj (get book :authors) new-author)]
+  (assoc book :authors new-authors)))
 
 (defn alive? [author]
-  :-)
+  (= nil (get author :death-year)))
 
 (defn element-lengths [collection]
-  :-)
+  (map count collection))
 
 (defn second-elements [collection]
   :-)
