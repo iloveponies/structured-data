467bef4dc447275acdf7b2122e9691081059e6ad
juho H <juhoth@ovi.com>
2013-10-13 21:56:24 +0300

    half way thru

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,25 @@
 (ns structured-data)
 
+;; (let [name1 value1
+;;       name2 value2
+;;       ...]
+;;   (expression1)
+;;   (expression2)
+;;   ...)
+
 (defn do-a-thing [x]
-  :-)
+  (let [summa (+ x x)]
+    (Math/pow summa summa)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[a _ b] v]
+    (+ a b))) 
 
 (defn point [x y]
   [x y])
@@ -19,22 +28,30 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (== (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [x y] point]
+    (and
+      (<= x1 x x2)
+      (<= y1 y y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (and
+    (contains-point? outer (get inner 0))
+    (contains-point? outer (get inner 1))))
 
 (defn title-length [book]
   :-)
