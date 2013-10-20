40f18a26101a6ff1cca93a17dc16af12752f37c8
Mikael Svenn <mikael.svenn@cs.helsinki.fi>
2013-10-20 20:44:32 +0300

    Done.

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -180,28 +180,47 @@
   )
 
 (defn authors->string [authors]
-  :-)
+  (apply str (interpose ", " (map author->string authors)))
+ )
 
 (defn book->string [book]
-  :-)
+  (apply str (:title book) ", written by " (authors->string (:authors book)))
+  )
 
 (defn books->string [books]
-  :-)
+  ; *brrrrr...*
+  (let [amount (count books)
+        selectBook (fn[book](book->string book))]
+   (cond(= 0 amount) "No books."
+    (= 1 amount) (str amount " book. " (apply book->string books)".")
+    :else (str amount " books. " (apply str (interpose ". "(map selectBook books)))".")
+    )
+  )
+)
 
 (defn books-by-author [author books]
-  :-)
+  (filter (fn [books] (has-author? books author)) books)
+  )
 
 (defn author-by-name [name authors]
-  :-)
+  (first (filter (fn [author] (= (:name author) name)) authors)
+  )
+)
 
 (defn living-authors [authors]
-  :-)
+  (filter (fn [author] (alive? author)) authors)
+  )
 
 (defn has-a-living-author? [book]
-  :-)
+  (not (empty? (living-authors (:authors book)))
+  )
+  )
 
 (defn books-by-living-authors [books]
-  :-)
+  (filter has-a-living-author? books)
+  )
 
 ; %________%
+
+
 
