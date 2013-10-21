ac71d054e8684fb502176d46ec37293d7c755e6a
Julppu <juha.lindqvist@cs.helsinki.fi>
2013-10-21 13:45:26 +0300

    progress

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+    (Math/pow xx xx)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z] v]
+    (+ x z)))
 
 (defn point [x y]
   [x y])
@@ -19,37 +21,44 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (= (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (* (width rectangle) (height rectangle))))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [x y] point]
+    (and (>= x x1) (<= x x2) (>= y y1) (<= y y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] inner
+        [[x3 y3] [x4 y4]] outer]
+    (and (<= x3 x1 x4) (<= x3 x2 x4) (<= y3 y1 y4) (<= y3 y2 y4))))
 
 (defn title-length [book]
-  :-)
+  (count (get book :title)))
 
 (defn author-count [book]
-  :-)
+  (count (get book :authors)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (count (:authors book)) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (assoc book :authors (conj (get book :authors) new-author)))
 
 (defn alive? [author]
-  :-)
+  (contains? author :death-year))
 
 (defn element-lengths [collection]
   :-)
@@ -111,4 +120,4 @@
 (defn books-by-living-authors [books]
   :-)
 
-; %________%
+; %________%
