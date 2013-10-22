5a05a42006431f73399660e4d80a5bb8b83629fb
rTonTTu <rTonTTu@gmail.com>
2013-10-22 15:47:40 +0300

    puoleen valiin

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,20 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [x x]
+    (Math/pow (+ x x) (+ x x)))
+  )
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[o k e] v]
+    (+ o e)))
+
 
 (defn point [x y]
   [x y])
@@ -19,37 +23,48 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[bl1 tr1] [bl2 tr2]] rectangle]
+  (- bl2 bl1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[bl1 tr1] [bl2 tr2]] rectangle]
+  (- tr2 tr1)))
 
 (defn square? [rectangle]
-  :-)
+  (if (== (height rectangle) (width rectangle)) true false ))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (if (and (<= (get (first rectangle) 0) (get point 0) (get (second rectangle) 0) )
+    (<= (get (first rectangle) 1) (get point 1) (get (second rectangle) 1) ) ) true false))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[inner-bottom-left inner-top-right] inner]
+    (and
+      (contains-point? outer inner-bottom-left)
+      (contains-point? outer inner-top-right))))
+
 
 (defn title-length [book]
-  :-)
+  (count (get book :title)))
 
 (defn author-count [book]
-  :-)
+  (count (get book :authors)))
 
 (defn multiple-authors? [book]
-  :-)
+  (if (> (count (get book :authors)) 1) true false))
 
 (defn add-author [book new-author]
-  :-)
+  (assoc book :authors
+  (conj (get book :authors) new-author)))
 
 (defn alive? [author]
-  :-)
+  (if (get author :death-year)
+    false
+    true))
+
 
 (defn element-lengths [collection]
   :-)
