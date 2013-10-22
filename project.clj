51ab3e8ef9611121320e95c07cd42bffaebf83c3
Lassi Kortela <lassi@lassikortela.net>
2013-10-23 01:09:18 +0300

    Solution

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject structured-data "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/math.numeric-tower "0.0.2"]
                  [midje-grader "0.1.0-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
                    :plugins [[lein-midje "3.1.1"]]}})
