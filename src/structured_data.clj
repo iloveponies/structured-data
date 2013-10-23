b55a4b7017328a50246044bed6682438f8b6757c
Juuso Strömberg <juuso.stromberg@cs.helsinki.fi>
2013-10-23 15:00:47 +0300

    Complete exercicises 1 - 12

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [x1 (+ x x)]
+  (Math/pow x1 x1)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z][(get v 0) (get v 1) (get v 2)]]
+    (+ x z)))
 
 (defn point [x y]
   [x y])
@@ -19,34 +21,41 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+      (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+      (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+      (== (- y2 y1) (- x2 x1))))
 
 (defn area [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+      (* (- y2 y1) (- x2 x1))))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[x3 y3] point]
+    (let [[[x1 y1] [x2 y2]] rectangle]
+          (and (<= x1 x3 x2) (<= y1 y3 y2)) )))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (and (contains-point? outer (get inner 1))
+       (contains-point? outer (get inner 0))))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (count (:authors book)) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (let [original ]))
 
 (defn alive? [author]
   :-)
