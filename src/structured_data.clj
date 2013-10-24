e5b076d12ff667afc7b85c1f8feb38d3ef4edf62
Janne Vento <jannejvento@gmail.com>
2013-10-24 22:49:53 +0300

    not..

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,22 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+    (Math/pow xx xx)
+  )
+  )
 
 (defn spiff [v]
-  :-)
+  (+ (get v 2) (get v 0))
+  )
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z] v]
+  (+ x z))
+  )
 
 (defn point [x y]
   [x y])
@@ -19,58 +25,101 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1))
+
+  )
 
 (defn height [rectangle]
-  :-)
+    (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1))
+
+  )
 
 (defn square? [rectangle]
-  :-)
+  (if (= (height rectangle) (width rectangle))
+    true
+    false)
+  )
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+     [x y] point]
+    (if (and (<= x1 x x2) (<= y1 y y2))
+      true
+      false)
+    )
+  )
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[px1 py1] [px2 py2]] inner]
+    (if (and (contains-point? outer [px1 py1]) (contains-point? outer [px2 py2]))
+      true
+      false
+    )
+  )
+  )
 
 (defn title-length [book]
-  :-)
+  (count (get book :title)))
 
 (defn author-count [book]
-  :-)
+  (count  (get book :authors))
+)
 
 (defn multiple-authors? [book]
-  :-)
-
+  (if(= (author-count book) 1)
+    false
+    true)
+)
 (defn add-author [book new-author]
-  :-)
+  (assoc book :authors  
+    (conj (:authors book) new-author))
+)
 
 (defn alive? [author]
-  :-)
+  (if (contains? author :death-year)
+    false
+    true)
+  )
 
 (defn element-lengths [collection]
-  :-)
+  (map count collection)
+)
 
 (defn second-elements [collection]
-  :-)
+  (let [seconds (fn [x] (get x 1))]
+    (map seconds collection)
+    ))
 
 (defn titles [books]
-  :-)
+  (map :title books))
 
 (defn monotonic? [a-seq]
-  :-)
-
+  (if (apply <= a-seq) true 
+    (if (apply >= a-seq) true
+      false))
+)
 (defn stars [n]
-  :-)
+  (apply str (repeat n "*"))
+)
 
 (defn toggle [a-set elem]
-  :-)
-
+  (if (contains? a-set elem)
+    (disj a-set elem)
+    (conj a-set elem)
+  )
+)
 (defn contains-duplicates? [a-seq]
-  :-)
+    (if (= (count (set a-seq)) (count a-seq))
+      false
+      true
+      )
+    )
+
 
 (defn old-book->new-book [book]
   :-)
@@ -112,3 +161,5 @@
   :-)
 
 ; %________%
+
+
