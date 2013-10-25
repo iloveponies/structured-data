17f5e192740337906a44d6c1c484f6b2dd9ce18a
santerim <santerim@cs.helsinki.fi>
2013-10-25 21:15:08 +0300

    Vikat. Riitti.

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -93,40 +93,75 @@
   )
 
 (defn monotonic? [a-seq]
-  :-)
+  (if (or (apply <= a-seq) (apply >= a-seq))
+    true
+    false)
+  )
 
 (defn stars [n]
-  :-)
+  (apply str(repeat n "*"))
+  )
 
 (defn toggle [a-set elem]
-  :-)
+  (if (contains? a-set elem)
+    (disj a-set elem)
+    (conj a-set elem))
+  )
 
 (defn contains-duplicates? [a-seq]
-  :-)
+  (if (= (count a-seq) (count (set a-seq)))
+    false
+    true)
+  )
 
 (defn old-book->new-book [book]
-  :-)
+  (assoc book :authors (set (book :authors)))
+  )
 
 (defn has-author? [book author]
-  :-)
+  (if (contains? (book :authors) author)
+    true
+    false)
+  )
 
 (defn authors [books]
-  :-)
+  (apply clojure.set/union (map :authors books))
+)
 
 (defn all-author-names [books]
-  :-)
+  (set (map :name (authors books)))
+)
 
 (defn author->string [author]
-  :-)
+  (let [getName (:name author)
+        getBirth (:birth-year author)
+        getDeath (:death-year author)]
+    (if (nil? getDeath)
+      (if (nil? getBirth)
+        (str getName)
+        (str getName " (" getBirth " - )"))
+      (str getName " (" getBirth " - " getDeath ")")
+    )
+  )
+)
 
 (defn authors->string [authors]
-  :-)
+  (apply str (interpose ", " (map author->string authors)))
+)
 
 (defn book->string [book]
-  :-)
+  (str(:title book) ", written by " (authors->string (:authors book)))
+)
 
 (defn books->string [books]
-  :-)
+  (if (= (count books) 0)
+    (str "No books.")
+    (if (= (count books) 1)
+      (apply str "1 book. " (map book->string books))
+      (apply str (count books) " books. " (interpose ". " (map book->string books)))
+    )
+  )
+)
 
 (defn books-by-author [author books]
   :-)
@@ -144,4 +179,6 @@
   :-)
 
 ; %________%
+
+
 
