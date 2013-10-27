48918a291dfb4a02def8e78d8b2b2cf921c3fc85
Lenni <lenni@live.com>
2013-10-27 15:26:08 +0200

    valiaika

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -164,10 +164,13 @@
 )
 
 (defn authors->string [authors]
-  :-)
+  (apply str (interpose ", " (map author->string authors))
+  )
+)
 
 (defn book->string [book]
-  :-)
+  (str (:title book) ", written by " (authors->string (:authors book)))
+)
 
 (defn books->string [books]
   :-)
