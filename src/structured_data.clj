b744cc5343120fed62f583cc9dbe9f623f96fede
Ilari Oras <happo1472583@gmail.com>
2014-01-02 23:02:40 +0200

    vektorei

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -22,22 +22,28 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x1 x2)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1 ] [x2 y2]] rectangle]
+    (- y1 y2)))
 
 (defn square? [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (and (= x1 x2) (= y1 y2))))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (let [[x y] point]
+      (and (<= x1 x x2) (<= y1 y y2)))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[x y] inner]
+    (and (contains-point? outer x) (contains-point? outer y))))
 
 (defn title-length [book]
   :-)
