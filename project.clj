a6b4256717d2efbf640663550fb9b762e5bf3caa
Gregg Williams <gregg13@innerpaths.net>
2014-01-18 23:55:54 -0800

    no errors

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject structured-data "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [iloveponies.tests/structured-data "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
+                 [iloveponies.tests/structured-data "0.1.0-SNAPSHOT"]
+                 [midje "1.6.0"]]
+;  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}}
+  :plugins [[lein-midje "3.1.3"]])
 
