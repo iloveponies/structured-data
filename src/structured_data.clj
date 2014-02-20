bb5062e13978f29a74ee2662d1a6ee0e2fb2cc1b
Toni Suominen <suominentoni@gmail.com>
2014-02-20 07:22:45 +0200

    Exercices 1 to 12 completed.

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -20,35 +20,41 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
+(defn width [rectangle] 
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (= (height rectangle)(width rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle)(width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [px py] point]
+    (and (>= px x1) (<= px x2) (>= py y1) (<= py y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[ox1 oy1] [ox2 oy2]] outer
+        [[ix1 iy1] [ix2 iy2]] inner]
+    (and (>= ix1 ox1) (>= iy1 oy1) (<= ix2 ox2) (<= iy2 oy2))))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (count (:authors book)) 1))
 
 (defn add-author [book new-author]
-  :-)
+  )
 
 (defn alive? [author]
   :-)
