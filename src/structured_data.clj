fbc2cfdeed5082ea2120dfd578c6357aada43bb5
iviv <ivaliy@gmail.com>
2014-02-25 15:03:59 -0800

    half tests

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,55 +1,63 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+    (Math/pow xx xx)))
+
 
 (defn spiff [v]
-  :-)
+ (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[a b c] v] (+ a c)))
 
-(defn point [x y]
-  [x y])
+(defn point [x y] [x y])
 
-(defn rectangle [bottom-left top-right]
-  [bottom-left top-right])
+(defn rectangle [bottom-left top-right]  [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+ (let [[[x1 y1][x2 y2]] rectangle]
+   (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+   (let [[[x1 y1][x2 y2]] rectangle ]
+     (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (= 0 (- (width rectangle) (height rectangle))))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1][x2 y2]] rectangle
+        [xx1 yy1] point]
+    (and (<= x1 xx1 x2) (<= y1 yy1 y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1][x2 y2]] outer
+        [[xx1 yy1][xx2 yy2]] inner]
+     (and (<= x1 xx1 xx2 x2) (<= y1 yy1 yy2 y2))
+ ))
 
 (defn title-length [book]
-  :-)
+  (count (get book :title)))
 
 (defn author-count [book]
-  :-)
+  (count(get book :authors)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (let [authors (get book :authors)]
+    (assoc book :authors (conj authors new-author))))
 
 (defn alive? [author]
-  :-)
+  (not (contains? author :death-year)))
 
 (defn element-lengths [collection]
   :-)
