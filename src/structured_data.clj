db291ac2c0b6b003212ca722849e96c2bc97c6d7
Grzegorz Jacykowski <nickt@o2.pl>
2014-06-02 21:48:43 +0200

    Temporary solution

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [ad (+ x x)]
+  (Math/pow ad ad)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z] v]
+  (+ x z)))
 
 (defn point [x y]
   [x y])
@@ -19,31 +21,47 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+  (- x2 x1))
+)
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+  (- y2 y1))
+)
 
 (defn square? [rectangle]
-  :-)
+  (if (= (width rectangle) (height rectangle))
+  true
+  false ))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+  [x3 y3] point]
+  (if (and (<= x1 x3 x2) (<= y1 y3 y2))
+  true
+  false)))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] inner]
+  (if (and (contains-point? outer (point x1 y1)) (contains-point? outer (point x2 y2)))
+  true
+false)))
+
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (if (> (author-count book) 1)
+  true
+  false))
 
 (defn add-author [book new-author]
   :-)
