4476a8d2851bf9eb82021ad71593786e028f1c9f
Tim Steinbach <tsteinbach@trustwave.com>
2014-07-03 09:01:08 -0400

    1-13

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+    (Math/pow xx xx)
+    ))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
-(defn spiff-destructuring [v]
-  :-)
+(defn spiff-destructuring [[e1 e2 e3 e4]]
+  (+ e1 e3))
 
 (defn point [x y]
   [x y])
@@ -18,35 +20,36 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
+(defn width [[[blx bly] [xtr ytr]]]
+  (- xtr blx))
 
-(defn height [rectangle]
-  :-)
+(defn height [[[blx bly] [xtr ytr]]]
+  (- ytr bly))
 
 (defn square? [rectangle]
-  :-)
+  (= (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
 
-(defn contains-point? [rectangle point]
-  :-)
+(defn contains-point? [[[blx bly] [xtr ytr]] [px py]]
+  (and (<= blx px) (>= xtr px) (<= bly py) (>= ytr py))
+  )
 
-(defn contains-rectangle? [outer inner]
-  :-)
+(defn contains-rectangle? [outer [ibl itr]]
+  (and (contains-point? outer ibl) (contains-point? outer itr)))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (assoc book :authors (conj (:authors book) new-author)))
 
 (defn alive? [author]
   :-)
