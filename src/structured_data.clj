f6cdf82b5254b13ad3eab8578a5be282abeb5b84
Chris Howe-Jones <chris.howejones@gmail.com>
2014-07-13 17:09:28 +0100

    Commit up to title-length implemented.

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,20 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [x-plus-x (+ x x)]
+    (Math/pow x-plus-x x-plus-x)))
 
 (defn spiff [v]
-  :-)
+  (+
+   (get v 0)
+   (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x _ y] v]
+    (+ x y)))
 
 (defn point [x y]
   [x y])
@@ -19,25 +23,37 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (let [h (height rectangle) w (width rectangle)]
+    (== h w)))
 
 (defn area [rectangle]
-  :-)
+  (let [h (height rectangle) w (width rectangle)]
+    (* h w)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[rx1 ry1] [rx2 ry2]] rectangle
+        [px py] point]
+    (and
+     (<= rx1 px rx2)
+     (<= ry1 py ry2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[inner-bot-left inner-top-right] inner]
+    (and
+     (contains-point? outer inner-bot-left)
+     (contains-point? outer inner-top-right))))
 
 (defn title-length [book]
-  :-)
+  (let [title (:title book)]
+    (count title)))
 
 (defn author-count [book]
   :-)
