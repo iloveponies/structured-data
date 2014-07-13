336cafef7cbf362e3ee853460d41562666013d80
Chris Howe-Jones <chris.howejones@gmail.com>
2014-07-13 17:27:33 +0100

    Implemented up to add-author

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -56,13 +56,16 @@
     (count title)))
 
 (defn author-count [book]
-  :-)
+  (let [authors (:authors book)]
+    (count authors)))
 
 (defn multiple-authors? [book]
-  :-)
+  (let [author-count (author-count book)]
+    (> author-count 1)))
 
 (defn add-author [book new-author]
-  :-)
+  (let [new-authors (conj (:authors book) new-author)]
+    (assoc book :authors new-authors)))
 
 (defn alive? [author]
   :-)
