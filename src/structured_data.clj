3697cddd95c9ebc92526ea13171580628c2b9a1a
Phil Businger <phil@businger.net>
2014-08-02 08:04:15 -0400

    not done yet

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,40 +1,54 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [double-x (+ x x)]
+    (Math/pow double-x double-x)))
 
 (defn spiff [v]
-  :-)
+  (if (and (coll? v) (> (count v) 2))
+    (+ (get v 0) (get v 2))))
 
 (defn cutify [v]
-  :-)
+  (if (vector? v) (conj v "<3")))
 
 (defn spiff-destructuring [v]
-  :-)
+(let [[x y z] v]
+  (+ x z)
+  ))
+
+(defn spiff-destructuring2 [[x y z]]
+  (+ x z)
+  )
 
 (defn point [x y]
   [x y])
+
 
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+(let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (= (height rectangle) (width rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+(let [[x3 y3] point
+      [[x1 y1][x2 y2]] rectangle]
+  (and (<= x1 x3 x2) (<= y1 y3 y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[p1 p2] inner]
+    (and (contains-point? outer p1) (contains-point? outer p2))))
 
 (defn title-length [book]
   :-)
