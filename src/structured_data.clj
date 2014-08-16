9327b470be663bc7ce0b3603ee26ceee15d85ae1
Alex Podaras <apodaras@gmail.com>
2014-08-16 19:48:56 +0300

    Solve exercise 28

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -104,7 +104,7 @@
   (apply str (interpose ", " (map author->string authors))))
 
 (defn book->string [book]
-  :-)
+  (str (:title book) ", written by " (authors->string (:authors book))))
 
 (defn books->string [books]
   :-)
