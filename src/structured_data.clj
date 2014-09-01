b350e50458e03a5721fdec0338e890defea28269
Aaro Lehikoinen <aaro.lehikoinen@cs.helsinki.fi>
2014-09-01 23:06:54 +0300

    first excercises

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,21 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let 
+    [doublex (+ x x)] 
+    (Math/pow doublex doublex)))
 
 (defn spiff [v]
-  :-)
+  (let 
+    [first (first v)
+     third (get v 2)]
+    (+ first third)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
-(defn spiff-destructuring [v]
-  :-)
+(defn spiff-destructuring [[fst _ trd]]
+  (+ fst trd))
 
 (defn point [x y]
   [x y])
@@ -18,35 +23,43 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
+(defn width [[[blx bly] [trx try]]]
+  (- trx blx))
 
-(defn height [rectangle]
-  :-)
+(defn height [[[blx bly] [trx try]]]
+  (- try bly))
 
 (defn square? [rectangle]
-  :-)
+  (= (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
-(defn contains-point? [rectangle point]
-  :-)
+(defn contains-point? [[[rblx rbly] [rtrx rtry]] [px py]]
+  (and 
+    (>= px rblx)
+    (<= px rtrx)
+    (>= py rbly)
+    (<= py rtry)))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[ibl itr] inner]
+  (and 
+    (contains-point? outer ibl)
+    (contains-point? outer itr))))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (assoc book :authors
+    (conj (:authors book) new-author)))
 
 (defn alive? [author]
   :-)
