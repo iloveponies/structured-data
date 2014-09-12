dde08dfdc9fa572d7026779ebc84bbd077921e36
Sampsa Laapotti <sampoz@iki.fi>
2014-09-13 00:54:48 +0300

    Yeah, this clojure is really handy stuff, feels almost like it writes itself :D

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,30 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (* x x)]
+    (Math/pow xx xx)
+   )
+  )
+
+(do-a-thing 2)
+()
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2))
+  )
 
+(spiff [1 2 3])
 (defn cutify [v]
-  :-)
+  (conj v "<3")
+  )
+(cutify [1])
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x z y]v]
+    (+ x y)
+  )
+  )
+(spiff-destructuring [1 2 3])
 
 (defn point [x y]
   [x y])
@@ -18,14 +32,33 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
-
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)
+    )
+  )
+9
+(defn width [rectangle]
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)
+    )
+  )
+(height (rectangle [1 1] [5 1])) ; 0
+(height (rectangle [1 1] [5 5])) ; 4
+(height (rectangle [0 0] [2 3])) ; 3
 
-(defn square? [rectangle]
-  :-)
+(width (rectangle [1 1] [5 1]))  ; 4
+(width (rectangle [1 1] [1 1]))  ; 0
+(width (rectangle [3 1] [10 4])) ; 7
+
+(defn square? [rec]
+  (if (= (height rec) (width rec)) true false)
+  )
+(square? (rectangle [1 1] [2 2])) ;=> true
+(square? (rectangle [1 1] [2 3])) ;=> false
+(square? (rectangle [1 1] [1 1])) ;=> true
+(square? (rectangle [3 2] [1 0])) ;=> true
+(square? (rectangle [3 2] [1 1])) ;=> false
 
 (defn area [rectangle]
   :-)
