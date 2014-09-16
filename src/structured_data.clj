9126bd724756eef78fe01975896943b91edf6fff
Michel Rasschaert <michel.rasschaert@gmail.com>
2014-09-17 00:53:05 +0200

    ch03-ex10-12

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -50,13 +50,13 @@
        (contains-point? outer pi2))))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (< 0 (author-count book)))
 
 (defn add-author [book new-author]
   :-)
