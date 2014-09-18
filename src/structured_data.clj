18983b46b8fd9c4f7527629c64bcb948106ba6b5
Mikko Koski <mikkokos@gmail.com>
2014-09-18 22:53:44 +0300

    Remove extra stuff

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,32 +1,5 @@
 (ns structured-data)
 
-(def china {:name "China Miéville", :birth-year 1972})
-(def octavia {:name "Octavia E. Butler"
-              :birth-year 1947
-              :death-year 2006})
-(def friedman {:name "Daniel Friedman" :birth-year 1944})
-(def felleisen {:name "Matthias Felleisen"})
-(def jrrtolkien {:name "J. R. R. Tolkien" :birth-year 1892 :death-year 1973})
-(def christopher {:name "Christopher Tolkien" :birth-year 1924})
-(def kay {:name "Guy Gavriel Kay" :birth-year 1954})
-
-(def silmarillion {:title "Silmarillion"
-                   :authors #{jrrtolkien, christopher, kay}})
-(def dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
-(def zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
-
-(def deus-irae {:title "Deus Irae", :authors #{dick, zelazny}})
-
-(def cities {:title "The City and the City" :authors #{china}})
-(def wild-seed {:title "Wild Seed", :authors #{octavia}})
-(def embassytown {:title "Embassytown", :authors #{china}})
-(def little-schemer {:title "The Little Schemer"
-                     :authors #{friedman, felleisen}})
-
-(def books [cities, wild-seed, embassytown, little-schemer])
-
-(def authors #{china, felleisen, octavia, friedman})
-
 (defn do-a-thing [x]
   (let [xx (+ x x)]
     (Math/pow xx xx)))
