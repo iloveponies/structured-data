81663e5dd925237fd1f623deebfdda6c807997cb
bogdan.radacina <bogdan.radacina@gmail.com>
2014-09-24 17:20:45 +1000

    solving some exercises

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,24 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+    (Math/pow xx xx)))
 
 (defn spiff [v]
-  :-)
+  (let [prem (get v 0)
+        third (get v 2)
+        sum 0]
+    (+ (if third third 0)
+      (if prem prem 0))))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[a b c] v]
+    (+
+     (if a a 0)
+     (if c c 0))))
 
 (defn point [x y]
   [x y])
@@ -18,17 +26,17 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
+(defn width [[[x1 y1] [x2 y2]]]
+  (Math/abs (- x1 x2)))
 
-(defn height [rectangle]
-  :-)
+(defn height [[[x1 y1] [x2 y2]]]
+  (Math/abs (- y1 y2)))
 
 (defn square? [rectangle]
-  :-)
+  (= (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
   :-)
