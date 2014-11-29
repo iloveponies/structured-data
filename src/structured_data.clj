c2e3f0ccb5efac5f95826f688b060d5d1dc9b249
Martin Hall <mjm.hall@gmail.com>
2014-11-29 11:01:57 +0000

    Initial commit

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,22 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [dbl-x (+ x x)]
+    (Math/pow dbl-x dbl-x)))
+
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
+
+
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
+
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x _ y] v]
+    (+ x y)))
 
 (defn point [x y]
   [x y])
@@ -19,28 +25,54 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
+
 
 (defn square? [rectangle]
-  :-)
+  (= (- (height rectangle) (width rectangle)) 0))
+
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
+
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [px1 py1] point]
+    (and (<= x1 px1 x2)
+         (<= y1 py1 y2))))
+
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] inner]
+    (and (contains-point? outer (point x1 y1))
+         (contains-point? outer (point x2 y2)))))
+
+
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def friedman {:name "Daniel Friedman" :birth-year 1944})
+(def felleisen {:name "Matthias Felleisen"})
+
+(def cities {:title "The City and the City" :authors [china]})
+(def wild-seed {:title "Wild Seed", :authors [octavia]})
+(def embassytown {:title "Embassytown", :authors [china]})
+(def little-schemer {:title "The Little Schemer"
+                     :authors [friedman, felleisen]})
 
 (defn title-length [book]
-  :-)
+  (count (get book :title)))
+
 
 (defn author-count [book]
-  :-)
+  (count (get book :authors)))
 
 (defn multiple-authors? [book]
   :-)
