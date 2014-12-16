93657e43a9d3ec3052605fd1425047a554821d41
jjelfs <jjelfs@travelrepublic.co.uk>
2014-12-16 16:06:32 +0000

    completed all exercises

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -56,66 +56,84 @@
         (assoc book :authors new-authors)))
 
 (defn alive? [author]
-  :-)
+  (not (contains? author :death-year)))
 
 (defn element-lengths [collection]
-  :-)
+  (map #(count %) collection))
 
 (defn second-elements [collection]
-  :-)
+  (map #(get % 1) collection))
 
 (defn titles [books]
-  :-)
+  (map :title books))
 
 (defn monotonic? [a-seq]
-  :-)
+  (or (apply <= a-seq) (apply >= a-seq)))
 
 (defn stars [n]
-  :-)
+  (apply str (repeat n "*")))
 
 (defn toggle [a-set elem]
-  :-)
+  (if (contains? a-set elem) 
+    (disj a-set elem)
+    (conj a-set elem)))
 
 (defn contains-duplicates? [a-seq]
-  :-)
+  (> (count a-seq) (count (set a-seq))))
 
 (defn old-book->new-book [book]
-  :-)
+  (assoc book :authors (set (:authors book))))
 
 (defn has-author? [book author]
-  :-)
+  (contains? (:authors book) author))
 
 (defn authors [books]
-  :-)
+  (apply clojure.set/union (map #(:authors %) books)))
 
 (defn all-author-names [books]
-  :-)
+  (let [a (authors books)]
+    (set (map #(:name %) a))))
+
+
+(defn life-span [author] 
+  (if (contains? author :birth-year) 
+    (if (contains? author :death-year) 
+      (str " (" (:birth-year author) " - " (:death-year author) ")")
+      (str " (" (:birth-year author) " - )"))
+    nil))
 
 (defn author->string [author]
-  :-)
+  (str (:name author) (life-span author)))
 
 (defn authors->string [authors]
-  :-)
+  (let [author-strings (map author->string authors)]
+    (apply str (interpose ", " author-strings))    ))
 
 (defn book->string [book]
-  :-)
+  (str (:title book) ", written by " (authors->string (:authors book))))
 
 (defn books->string [books]
-  :-)
+  (let [book-strings (map book->string books)
+        parsed (apply str (interpose ", " book-strings))
+        num (count books)
+        label (if (> num 1) " books. " " book. ")]
+    (if (> num 0)
+      (str num label parsed ".")
+      "No books.")))
 
 (defn books-by-author [author books]
-  :-)
+  (filter #(has-author? % author) books))
 
 (defn author-by-name [name authors]
-  :-)
+  (first (filter #(= name (:name %)) authors)))
 
 (defn living-authors [authors]
-  :-)
+  (filter alive? authors))
 
 (defn has-a-living-author? [book]
-  :-)
+  (not (empty? (living-authors (:authors book)))))
 
 (defn books-by-living-authors [books]
-  :-)
+  (filter has-a-living-author? books))
 
 ; %________%
