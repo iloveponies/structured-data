a68ffc1ff378022f0bb69688f569f84cd020da61
samutamm <samutamm@gmail.com>
2014-12-20 19:58:01 +0200

    Exercises 1-8.

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,37 +1,52 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [doubleX (+ x x)]
+  (Math/pow doubleX doubleX)))
 
 (defn spiff [v]
-  :-)
+  (cond
+   (> (count v) 2) (+ (get v 0) (get v 2))
+   :else "?"))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (cond
+   (> (count v) 2)
+     (let [[a b c] v ] (+ a c))
+   :else "?"))
 
 (defn point [x y]
   [x y])
 
+(defn abs [x]
+  (cond
+   (>= x 0) x
+   :else (- x)))
+
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
-
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1][x2 y2]] rectangle]
+  (abs (- y1 y2))))
+
+(defn width [rectangle]
+  (let [[[x1 y1][x2 y2]] rectangle]
+  (abs (- x1 x2))))
 
 (defn square? [rectangle]
-  :-)
+  (= (width rectangle)(height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle)(width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1][x2 y2]] rectangle]
+    (let [[pointX pointY] point]
+      (and (<= x1 pointX x2) (<= y1 pointY y2)))))
 
 (defn contains-rectangle? [outer inner]
   :-)
