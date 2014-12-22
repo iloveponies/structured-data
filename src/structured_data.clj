7157777f5da51cb485a803b1b1a7c176cffa08f9
Dexter Lowe <dexter.lowe@imagini.net>
2014-12-22 18:43:38 +0000

    EX-28 Implemented function to stringify a book

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -118,7 +118,7 @@
   (apply str (interpose ", " (map author->string authors))))
 
 (defn book->string [book]
-  :-)
+  (str (:title book) ", written by " (authors->string (:authors book))))
 
 (defn books->string [books]
   :-)
