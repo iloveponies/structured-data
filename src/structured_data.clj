a41ad6d27456794bab160cc0174514d4b54a7b3c
Enrique M. Talavera <kiquetal@kathy.site>
2015-01-04 16:57:23 -0300

    new exercises

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,14 @@
 (ns structured-data)
 
-(defn do-a-thing [x]
-  :-)
+(defn do-a-thing [x] (let [sum (+ x x)] (Math/pow sum sum)))
 
-(defn spiff [v]
-  :-)
 
-(defn cutify [v]
-  :-)
+(defn spiff [x] (+ (get x 2) (get x 0)))
 
-(defn spiff-destructuring [v]
-  :-)
+(defn cutify [x] (conj x "<3"))
+
+
+(defn spiff-destructuring [[x1 x2 x3]] (+ x1 x3))
 
 (defn point [x y]
   [x y])
@@ -18,17 +16,18 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
 
-(defn height [rectangle]
-  :-)
+(defn width [[[x1 y1] [x2 y2]]] (- x2 x1))
 
-(defn square? [rectangle]
-  :-)
+(defn height [[[x1 y1] [x2 y2]]] (- y2 y1))
 
-(defn area [rectangle]
-  :-)
+(defn abs [x] (if (< x 0) (* x -1) x))
+
+(defn square? [[rec1 rec2]] (let [[x1 y1] rec1 [x2 y2] rec2] (= (abs(- x1 x2)) (abs(- y2 y1)))))
+
+
+(defn area [[rec1 rec2]] (let [[x1 y1] rec1 [x2 y2] rec2] (* (abs(- y2 y1)) (abs(- x2 x1)))))
+
 
 (defn contains-point? [rectangle point]
   :-)
