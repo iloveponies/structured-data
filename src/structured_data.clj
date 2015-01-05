8990fc44bfa6bd4a0e58d2e22910b11e3fb08bba
Julian Mayorga <mayorga.julian@gmail.com>
2015-01-04 23:48:35 -0300

    Solve exercises 1-14

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,21 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [x+x (+ x x)]
+  (Math/pow x+x x+x)))
 
 (defn spiff [v]
-  :-)
+  "Returns the sum of the first and third elements of a vector"
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  "Adds <3 to the end of a vector"
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  "Returns the sum of the first and third elements of a vector-DESTRUCTURE STYLE!"
+  (let [[x y z] v]
+    (+ x z)))
 
 (defn point [x y]
   [x y])
@@ -19,37 +24,48 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (if (= (width rectangle) (height rectangle)) true false))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  "Returns true if rectangle contains point and otherwise false"
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [px py] point]
+  (if (and (<= x1 px x2) (<= y1 py y2)) true false)))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  "Returns true if the rectangle inner is inside the rectangle outer and otherwise false"
+  (let [[ix iy] inner]
+    (if (and (contains-point? outer ix)
+           (contains-point? outer iy)) true false)))
 
 (defn title-length [book]
-  :-)
+  (count (get book :title)))
 
 (defn author-count [book]
-  :-)
+  (count (get book :authors)))
 
 (defn multiple-authors? [book]
-  :-)
+  (if (>= (author-count book) 2) true false))
 
 (defn add-author [book new-author]
-  :-)
+  "Takes a book and an author and adds author to books authors"
+  (let [authors (get book :authors)
+        updated-authors (conj authors new-author)]
+    (assoc book :authors updated-authors)))
 
 (defn alive? [author]
-  :-)
+  (if (contains? author :death-year) false true))
 
 (defn element-lengths [collection]
   :-)
