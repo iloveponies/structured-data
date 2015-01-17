76cadf5eaa1394c9fc0bdb73171e5435c2ea454a
jykke <jyri.lehva@saunalahti.fi>
2015-01-17 23:36:18 +0200

    9

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -44,7 +44,10 @@
      (<= y1 py y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[p1 p2] inner]
+    (and
+      (contains-point? outer p1)
+      (contains-point? outer p2))))
 
 (defn title-length [book]
   :-)
