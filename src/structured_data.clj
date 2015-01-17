409520e2956a82bc696e9761fe739c25700a9652
Theo Reyes <vincintz@yahoo.com>
2015-01-17 22:38:39 +0800

    81 v 50

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,20 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [x2 (+ x x)]
+    (Math/pow x2 x2)))
 
 (defn spiff [v]
-  :-)
+  (let [first (get v 0)
+        third (get v 2)]
+    (+ first third)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x _ z] v]
+    (+ x z)))
 
 (defn point [x y]
   [x y])
@@ -19,19 +23,28 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[left _] [right _]] rectangle]
+    (- right left)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[_ bottom] [_ top]] rectangle]
+    (- top bottom)))
 
 (defn square? [rectangle]
-  :-)
+  (let [w (width  rectangle)
+        h (height rectangle)]
+    (= w h)))
 
 (defn area [rectangle]
-  :-)
+  (let [w (width  rectangle)
+        h (height rectangle)]
+    (* w h)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[x y] point
+        [[x1 y1] [x2 y2]] rectangle]
+    (and (<= x1 x x2)
+         (<= y1 y y2))))
 
 (defn contains-rectangle? [outer inner]
   :-)
