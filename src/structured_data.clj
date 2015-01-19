c2d1b41c15b5a5816a3d63ceaa4a9dbd4e2055d4
Theo Reyes <vincintz@yahoo.com>
2015-01-19 23:01:47 +0800

    46 checks failed, 85 succeeded.

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -86,7 +86,9 @@
   (apply str (repeat n "*")))
 
 (defn toggle [a-set elem]
-  :-)
+  (if (contains? a-set elem)
+    (disj a-set elem)
+    (conj a-set elem)))
 
 (defn contains-duplicates? [a-seq]
   :-)
