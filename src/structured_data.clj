f5e003633bed547f9ff5998bf0d887c134148b02
Pete <pmmakinen@hotmail.com>
2015-01-20 23:30:11 +0200

    more than half done

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [y (+ x x)]
+    (Math/pow y y)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z] v]
+    (+ x z)))
 
 (defn point [x y]
   [x y])
@@ -19,52 +21,73 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (if (= (width rectangle) (height rectangle))
+    true
+    false))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[x y] point]
+    (let [[[x1 y1] [x2 y2]] rectangle]
+
+      (if (and (<= x1 x x2) (<= y1 y y2))
+        true
+        false
+  ))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let[[[inner_x1 inner_y1] [inner_x2 inner_y2]] inner]
+        (let [bottom-left (point inner_x1 inner_y1)]
+          (let [top-right (point inner_x2 inner_y2)]
+             (and (contains-point? outer bottom-left) (contains-point? outer top-right))
+  ))))
+
 
 (defn title-length [book]
-  :-)
+  (count (:title book))
+  )
+
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (let [auths (conj (:authors book) new-author)]
+    (assoc book :authors auths )))
 
 (defn alive? [author]
-  :-)
+  (not (contains? author :death-year)))
 
 (defn element-lengths [collection]
-  :-)
+  (map count collection))
+
 
 (defn second-elements [collection]
-  :-)
+  (let [second (fn [x] (get x 1))]
+    (map second collection)))
+
 
 (defn titles [books]
-  :-)
+  (map :title books))
 
 (defn monotonic? [a-seq]
-  :-)
+  (or (apply <= a-seq) (apply >= a-seq)))
 
 (defn stars [n]
-  :-)
+  (apply str(repeat n \*)))
 
 (defn toggle [a-set elem]
   :-)
