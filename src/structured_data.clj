1edd3eee772d9551f8853965dd6fb07910db245b
41407 <jimi.welling@cs.helsinki.fi>
2015-04-26 20:59:28 +0300

    Exercise 13

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -58,7 +58,9 @@
   (> (author-count book) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (let [original (:authors book)
+        new-authors (conj (:authors book) new-author)]
+        (assoc book :authors new-authors)))
 
 (defn alive? [author]
   :-)
