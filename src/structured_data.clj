cf173c255198605c59a8a7df7eedff928f94f150
41407 <jimi.welling@cs.helsinki.fi>
2015-04-26 21:00:19 +0300

    Remove unused let from ex13

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -58,8 +58,7 @@
   (> (author-count book) 1))
 
 (defn add-author [book new-author]
-  (let [original (:authors book)
-        new-authors (conj (:authors book) new-author)]
+  (let [new-authors (conj (:authors book) new-author)]
         (assoc book :authors new-authors)))
 
 (defn alive? [author]
