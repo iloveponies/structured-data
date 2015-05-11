83fd7905a100e5fc1ad3d9812ef8e8d831974ecc
Saqib Saleem (EE) <ssaleem@equalexperts.com>
2015-05-11 21:49:48 +0100

    Solutions to structured-data - WIP

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,23 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [x2 (+ x x)]
+    (Math/pow x2 x2)))
 
 (defn spiff [v]
-  :-)
+  (if (> (count v) 2)
+    (+ (get v 0) (get v 2))
+    nil))
+
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (if (> (count v) 2)
+    (let [[a b c] v]
+      (+ a c))
+    nil))
 
 (defn point [x y]
   [x y])
@@ -19,46 +26,72 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[blx bly] [trx try]] rectangle]
+    (- trx blx)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[blx bly] [trx try]] rectangle]
+    (- try bly)))
 
 (defn square? [rectangle]
-  :-)
+  (= (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[blx bly] [trx try]] rectangle
+        [x y] point]
+       (and (<= blx x trx) (<= bly y try))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[bl tr] inner]
+    (and (contains-point? outer bl) (contains-point? outer tr))))
+
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def friedman {:name "Daniel Friedman" :birth-year 1944})
+(def felleisen {:name "Matthias Felleisen"})
+
+(def cities {:title "The City and the City" :authors [china]})
+(def wild-seed {:title "Wild Seed", :authors [octavia]})
+(def embassytown {:title "Embassytown", :authors [china]})
+(def little-schemer {:title "The Little Schemer"
+                     :authors [friedman, felleisen]})
+
+(assoc cities :awards ["Hugo", "World Fantasy Award",
+                       "Arthur C. Clarke Award",
+                       "British Science Fiction Award"])
+
+(def all-books [cities, wild-seed, embassytown, little-schemer])
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (let [new-authors (conj (:authors book) new-author)]
+    (assoc book :authors new-authors)))
 
 (defn alive? [author]
-  :-)
+  (not (contains? author :death-year)))
 
 (defn element-lengths [collection]
-  :-)
+  (map count collection))
 
 (defn second-elements [collection]
-  :-)
+  (let [second-element (fn [x] (first (rest x)))]
+    (map second-element collection)))
 
 (defn titles [books]
-  :-)
+  (map :title books))
 
 (defn monotonic? [a-seq]
   :-)
