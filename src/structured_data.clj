9891ac6a12038fc9800f2451ac03d57a4f346dff
Saqib Saleem (EE) <ssaleem@equalexperts.com>
2015-05-12 11:36:19 +0100

    Solutions to structured-data - WIP#2

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -42,24 +42,24 @@
 (defn contains-point? [rectangle point]
   (let [[[blx bly] [trx try]] rectangle
         [x y] point]
-       (and (<= blx x trx) (<= bly y try))))
+    (and (<= blx x trx) (<= bly y try))))
 
 (defn contains-rectangle? [outer inner]
   (let [[bl tr] inner]
     (and (contains-point? outer bl) (contains-point? outer tr))))
 
 (def china {:name "China Miéville", :birth-year 1972})
-(def octavia {:name "Octavia E. Butler"
+(def octavia {:name       "Octavia E. Butler"
               :birth-year 1947
               :death-year 2006})
 (def friedman {:name "Daniel Friedman" :birth-year 1944})
 (def felleisen {:name "Matthias Felleisen"})
 
-(def cities {:title "The City and the City" :authors [china]})
-(def wild-seed {:title "Wild Seed", :authors [octavia]})
-(def embassytown {:title "Embassytown", :authors [china]})
-(def little-schemer {:title "The Little Schemer"
-                     :authors [friedman, felleisen]})
+(def cities {:title "The City and the City" :authors #{china}})
+(def wild-seed {:title "Wild Seed", :authors #{octavia}})
+(def embassytown {:title "Embassytown", :authors #{china}})
+(def little-schemer {:title   "The Little Schemer"
+                     :authors #{friedman, felleisen}})
 
 (assoc cities :awards ["Hugo", "World Fantasy Award",
                        "Arthur C. Clarke Award",
@@ -94,40 +94,60 @@
   (map :title books))
 
 (defn monotonic? [a-seq]
-  :-)
+  (or (apply <= a-seq) (apply >= a-seq)))
 
 (defn stars [n]
-  :-)
+  (apply str (repeat n \*)))
 
 (defn toggle [a-set elem]
-  :-)
+  (if (contains? a-set elem)
+    (disj a-set elem)
+    (conj a-set elem)))
 
 (defn contains-duplicates? [a-seq]
-  :-)
+  (let [a-set (set a-seq)]
+    (not (= (count a-set) (count a-seq)))))
 
 (defn old-book->new-book [book]
-  :-)
+  (assoc book :authors (set (:authors book))))
 
 (defn has-author? [book author]
-  :-)
+  (contains? (:authors book) author))
+
+(defn author-names [book]
+  (map :name (:authors book)))
 
 (defn authors [books]
-  :-)
+  (clojure.set/union (set (apply concat (map :authors books)))))
 
 (defn all-author-names [books]
-  :-)
+  (set (map :name (authors books))))
 
 (defn author->string [author]
-  :-)
+  (let [name (:name author)
+        by (:birth-year author)
+        dy (:death-year author)
+        dates (if dy
+                (str " (" by " - " dy ")")
+                (if by
+                  (str " (" by " - )")
+                  ""))]
+    (str name dates)
+    ))
 
 (defn authors->string [authors]
-  :-)
+  (apply str( interpose ", " (map author->string authors))) )
 
 (defn book->string [book]
-  :-)
+  (str (:title book) (str ", written by ") (authors->string (:authors book))))
 
 (defn books->string [books]
-  :-)
+  (let [book-count (count books)]
+    (if (= book-count 0)
+      (str "No books.")
+      (str (if (= book-count 1)
+        (str book-count " book. ")
+        (str book-count " books. ")) (apply str (interpose ". " (map book->string books))) "."))))
 
 (defn books-by-author [author books]
   :-)
