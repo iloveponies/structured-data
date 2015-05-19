e909031ec38076609ae75831294417f8de651e16
ybykov <aS5bPr6n>
2015-05-19 21:35:53 +0200

    not yet done

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+  (Math/pow xx xx)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[a b c] v]
+    (+ a c)))
 
 (defn point [x y]
   [x y])
@@ -19,96 +21,105 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (if (> x1 x2) (- x1 x2) (- x2 x1))))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (if (> y1 y2) (- y1 y2) (- y2 y1))))
 
 (defn square? [rectangle]
-  :-)
+  (= (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle [x y] point]
+    (and (<= x1 x x2) (<= y1 y y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [_ outer [p1 p2] inner]
+    (and (contains-point? outer p1) (contains-point? outer p2))))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (let [authors (:authors book)
+        updated-authors (conj authors new-author)]
+    (assoc book :authors updated-authors)))
+
 
 (defn alive? [author]
-  :-)
+  (not (contains? author :death-year)))
 
 (defn element-lengths [collection]
-  :-)
+  (map count collection))
 
 (defn second-elements [collection]
-  :-)
+    (let [se (fn [x] (get x 1))]
+    (map se collection)))
 
 (defn titles [books]
-  :-)
+  (let [title (fn [book] (:title book))]
+    (map title books)))
 
 (defn monotonic? [a-seq]
-  :-)
+  ":-)")
 
 (defn stars [n]
-  :-)
+  ":-)")
 
 (defn toggle [a-set elem]
-  :-)
+  ":-)")
 
 (defn contains-duplicates? [a-seq]
-  :-)
+  ":-)")
 
 (defn old-book->new-book [book]
-  :-)
+  ":-)")
 
 (defn has-author? [book author]
-  :-)
+  ":-)")
 
 (defn authors [books]
-  :-)
+  ":-)")
 
 (defn all-author-names [books]
-  :-)
+  ":-)")
 
 (defn author->string [author]
-  :-)
+  ":-)")
 
 (defn authors->string [authors]
-  :-)
+  ":-)")
 
 (defn book->string [book]
-  :-)
+  ":-)")
 
 (defn books->string [books]
-  :-)
+  ":-)")
 
 (defn books-by-author [author books]
-  :-)
+  ":-)")
 
 (defn author-by-name [name authors]
-  :-)
+  ":-)")
 
 (defn living-authors [authors]
-  :-)
+  ":-)")
 
 (defn has-a-living-author? [book]
-  :-)
+  ":-)")
 
 (defn books-by-living-authors [books]
-  :-)
+  ":-)")
 
 ; %________%
