549afc16be3b2b057de5544c7db936316e70362a
ybykov <yury.bykov@gmail.com>
2015-05-20 16:29:31 +0200

    finally done!

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -79,48 +79,57 @@
   (apply str (repeat n "*")))
 
 (defn toggle [a-set elem]
-  ":-)")
+  (if (contains? a-set elem) (disj a-set elem) (conj a-set elem)))
 
 (defn contains-duplicates? [a-seq]
-  ":-)")
+  (not= (count a-seq) (count (set a-seq))))
 
 (defn old-book->new-book [book]
-  ":-)")
+  (let [authors (:authors book)
+        updated-authors (set authors)]
+    (assoc book :authors updated-authors)))
 
 (defn has-author? [book author]
-  ":-)")
+  (contains? (:authors book) author))
 
 (defn authors [books]
-  ":-)")
+(apply clojure.set/union (map :authors books)))
 
 (defn all-author-names [books]
-  ":-)")
+  (set (map :name (authors books))))
 
 (defn author->string [author]
-  ":-)")
+  (let [name (:name author)
+        years (if (contains? author :birth-year) (str " (" (:birth-year author) " - " (if (contains? author :death-year) (:death-year author)) ")"))]
+    (str name years)))
 
 (defn authors->string [authors]
-  ":-)")
+  (apply str (interpose ", " (map author->string authors))))
 
 (defn book->string [book]
-  ":-)")
+  (let [title (:title book)]
+    (str title ", written by " (authors->string (:authors book)))))
 
 (defn books->string [books]
-  ":-)")
+  (let [len (count books)]
+    (cond
+     (= len 0) "No books."
+     (= len 1) (str "1 book. " (book->string (first books)) ".")
+     :else (str len " books. " (apply str (interpose ". " (map book->string books))) "."))))
 
 (defn books-by-author [author books]
-  ":-)")
+  (filter (fn [book] (has-author? book author)) books))
 
 (defn author-by-name [name authors]
-  ":-)")
+  (first (filter (fn [author] (= (:name author) name)) authors)))
 
 (defn living-authors [authors]
-  ":-)")
+  (filter alive? authors))
 
 (defn has-a-living-author? [book]
-  ":-)")
+  (not (empty? (living-authors (:authors book)))))
 
 (defn books-by-living-authors [books]
-  ":-)")
+  (filter has-a-living-author? books))
 
 ; %________%
