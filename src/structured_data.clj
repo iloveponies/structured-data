0a036cf737237dc4869ae8445c535d19d23de312
tiaros <timo.a.jaaskelainen@helsinki.fi>
2015-06-25 15:54:03 +0300

    structured data

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,24 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x )]
+
+  (Math/pow xx xx))
+  )
 
 (defn spiff [v]
-  :-)
+  (let [a (get v 1)
+        b (get v 3)]
+    (+ a b)
+  )
+  )
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+   (let [[x y z] v]
+     (+ x z)))
 
 (defn point [x y]
   [x y])
@@ -37,49 +45,64 @@
   :-)
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (cond
+    (= 1 (author-count book)) false
+   :else true
+   ))
 
 (defn add-author [book new-author]
-  :-)
+  (assoc book :authors (conj (book :authors) new-author)))
 
 (defn alive? [author]
-  :-)
+  (not (contains? author :death-year)))
 
 (defn element-lengths [collection]
-  :-)
+
+  )
 
 (defn second-elements [collection]
-  :-)
+ (map second collection)
+)
 
 (defn titles [books]
-  :-)
+  (map :title books))
 
 (defn monotonic? [a-seq]
-  :-)
+  (cond
+   (apply >= a-seq) true
+   (apply <= a-seq) true
+   :else false
+   )
+    )
 
 (defn stars [n]
-  :-)
+  (apply str (repeat n "*"))
+  )
 
 (defn toggle [a-set elem]
-  :-)
+  (cond
+  (contains? a-set elem) (disj a-set elem)
+   :else (conj a-set elem))
+)
 
 (defn contains-duplicates? [a-seq]
-  :-)
+  (if (= (count a-seq) (count (set a-seq))) false true))
 
 (defn old-book->new-book [book]
-  :-)
+)
 
 (defn has-author? [book author]
-  :-)
+  (contains? (book :authors) author))
 
 (defn authors [books]
-  :-)
+  (set (apply concat (map :authors books)))
+)
 
 (defn all-author-names [books]
   :-)
