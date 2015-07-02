97e53ff098fced0bfaaae129914bfcebc99a4a19
Tiaros <timo.a.jaaskelainen@helsinki.fi>
2015-07-02 12:00:13 +0300

    Ready

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -2,30 +2,37 @@
 
 (defn do-a-thing [x]
   (let [xx (+ x x )]
-
   (Math/pow xx xx))
   )
 
 (defn spiff [v]
-   (+ (get v 1) (get v 3))
+   (let [f (get v 0)
+         t (get v 2)]
+     (+ f t)
+   )
   )
 
 (defn cutify [v]
-  (conj v "<3"))
+  (conj v "<3")
+  )
 
 (defn spiff-destructuring [v]
    (let [[x y z] v]
-     (+ x z)))
+     (+ x z))
+  )
 
 (defn point [x y]
-  [x y])
+  [x y]
+  )
 
 (defn rectangle [bottom-left top-right]
-  [bottom-left top-right])
+  [bottom-left top-right]
+  )
 
 (defn width [rectangle]
    (let [[[x1 y1] [x2 y2]] rectangle]
-    (- x2 x1)))
+    (- x2 x1))
+  )
 
 (defn height [rectangle]
   (let [[[x1 y1] [x2 y2]] rectangle]
@@ -51,29 +58,33 @@
           [[x3 y3] [x4 y4]] inner]
         (if (and (<= x1 x3) (>= x2 x4) (<= y1 y3) (>= y2 y4)) true false)
         )
-
   )
 
 (defn title-length [book]
-  (count (:title book)))
+  (count (:title book))
+  )
 
 (defn author-count [book]
-  (count (:authors book)))
+  (count (:authors book))
+  )
 
 (defn multiple-authors? [book]
   (cond
     (= 1 (author-count book)) false
    :else true
-   ))
+   )
+  )
 
 (defn add-author [book new-author]
-  (assoc book :authors (conj (book :authors) new-author)))
+  (assoc book :authors (conj (book :authors) new-author))
+  )
 
 (defn alive? [author]
-  (not (contains? author :death-year)))
+  (not (contains? author :death-year))
+  )
 
 (defn element-lengths [collection]
-
+(map count collection)
   )
 
 (defn second-elements [collection]
@@ -105,6 +116,7 @@
   (if (= (count a-seq) (count (set a-seq))) false true))
 
 (defn old-book->new-book [book]
+  (assoc book :authors (set (:authors book)))
 )
 
 (defn has-author? [book author]
@@ -115,33 +127,64 @@
 )
 
 (defn all-author-names [books]
-  (set (apply concat (map :authors books))))
+  (set (map :name (authors books))))
 
 (defn author->string [author]
-  :-)
+  (let [years (if (contains? author :birth-year)
+     (str " (" (:birth-year author) " - " (:death-year author) ")")
+                nil)]
+    (str (:name author) years))
+    )
 
 (defn authors->string [authors]
-  :-)
+  (apply str (interpose ", " (map author->string authors)))
+)
 
-(defn book->string [book]
-  :-)
+ (defn book->string [book]
+  (let [writer (if (contains? book :authors)
+                (str ", written by " (authors->string (:authors book))
+                ) nil)]
+  (str (:title book) writer))
+  )
 
 (defn books->string [books]
-  :-)
+  (let [amount (count books)
+
+        number (cond
+                (== 0 amount) "No books."
+                (== 1 amount) "1 book."
+                :else (str amount " books."))
+
+        books-string (map book->string books)
+        books-with-dots (interpose ". " books-string)
+        books-stringed (apply str books-with-dots)
+        ready-coll (if (< 0 amount) (str " " books-stringed ".") books-stringed)
+        ]
+     (str number ready-coll)
+    )
+)
 
 (defn books-by-author [author books]
-  :-)
+    (let [has-this-author? (fn [book-list] (has-author? book-list author))]
+        (filter has-this-author? books))
+  )
+
 
 (defn author-by-name [name authors]
-  :-)
+    (let [persons-name? (fn [person] (if (= (:name person) name) true false))]
+        (first (filter persons-name? authors)))
+  )
 
 (defn living-authors [authors]
-  :-)
+  (filter alive? authors)
+  )
 
 (defn has-a-living-author? [book]
-  :-)
+  (not (empty? (living-authors (:authors book))))
+  )
 
 (defn books-by-living-authors [books]
-  :-)
+  (filter has-a-living-author? books)
+  )
 
 ; %________%
