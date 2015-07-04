8e9f2ff9bf2f2d0902dafa2a324ee8eeee9d1df1
Tarmo Räntilä <tarmo.j.rantila@gmail.com>
2015-07-04 20:50:56 +0300

    Look-up, map and apply related function implementations.

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -44,34 +44,39 @@
             (contains-point? outer inner-point)) inner))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (let [authors (:authors book)]
+    (assoc book :authors
+      (conj authors new-author))))
 
 (defn alive? [author]
-  :-)
+  (not (contains? author :death-year)))
 
 (defn element-lengths [collection]
-  :-)
+  (map count collection))
 
 (defn second-elements [collection]
-  :-)
+  ; Built-in (second coll) would have done the job as well, supposedly.
+  (let [pick-second (fn [collection] (get collection 1))]
+    (map pick-second collection)))
 
 (defn titles [books]
-  :-)
+  (map :title books))
 
 (defn monotonic? [a-seq]
-  :-)
+  (or (apply <= a-seq)
+      (apply >= a-seq)))
 
 (defn stars [n]
-  :-)
+  (apply str (repeat n "*")))
 
 (defn toggle [a-set elem]
   :-)
