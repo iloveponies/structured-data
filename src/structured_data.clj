9a5dd41d7116a62ed8a401dbcafc0ccc4438de5b
Tarmo Räntilä <tarmo.j.rantila@gmail.com>
2015-07-04 22:17:39 +0300

    The rest of aggregate actions.

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -123,18 +123,18 @@
        "."))
 
 (defn books-by-author [author books]
-  :-)
+  (filter (fn [book] (has-author? book author)) books))
 
 (defn author-by-name [name authors]
-  :-)
+  (first (filter (fn [author] (= (:name author) name)) authors)))
 
 (defn living-authors [authors]
-  :-)
+  (filter alive? authors))
 
 (defn has-a-living-author? [book]
-  :-)
+  (not (empty? (living-authors (:authors book)))))
 
 (defn books-by-living-authors [books]
-  :-)
+  (filter has-a-living-author? books))
 
 ; %________%
