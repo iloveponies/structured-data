e12f268e9428dffdf3e48bfb3767ab670b6d3a20
Tarmo Räntilä <tarmo.j.rantila@gmail.com>
2015-07-04 20:17:11 +0300

    Rectangle's area, contains-point? and contains-rectangle?.

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -32,13 +32,16 @@
   (= (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [x y] point]
+    (and (<= x1 x x2) (<= y1 y y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (every? (fn [inner-point]
+            (contains-point? outer inner-point)) inner))
 
 (defn title-length [book]
   :-)
