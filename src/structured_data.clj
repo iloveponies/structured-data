1223d3f96d4d9d32845991699101f6c74be2feb1
lemanne <ilari.nieminen@outlook.com>
2015-07-06 17:22:13 +0300

    teht. 1-14

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,28 @@
 (ns structured-data)
 
+
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)
+        ]
+        (Math/pow xx xx)
+  )
+
+)
+
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2))
+
+
+  )
 
 (defn cutify [v]
-  :-)
+  (conj v "<3")
 
-(defn spiff-destructuring [v]
-  :-)
+   )
+
+(defn spiff-destructuring [[x1 x2 x3]]
+  (+ x1 x3))
 
 (defn point [x y]
   [x y])
@@ -19,37 +31,102 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+
+            (- x2 x1)
+
+  )
+)
 
 (defn height [rectangle]
-  :-)
+
+  (let [[[x1 y1] [x2 y2]] rectangle]
+
+            (- y2 y1)
+
+  )
+
+
+)
 
 (defn square? [rectangle]
-  :-)
+
+  (if (= (height rectangle) (width rectangle)) true false)
+
+
+)
 
 (defn area [rectangle]
-  :-)
+
+  (* (height rectangle) (width rectangle))
+
+
+
+)
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (let [[px py] point]
+
+    (cond
+        (and (<= x1 px x2) (<= y1 py y2)) true
+        :else false
+
+
+
+
+    )
+
+  )
+  )
+
+)
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] inner]
+    (let [[[ox1 oy1] [ox2 oy2]] outer]
+
+    (cond
+
+     (and (contains-point? (rectangle [ox1 oy1] [ox2 oy2]) (point x1 y1)) (contains-point? (rectangle [ox1 oy1] [ox2 oy2]) (point x2 y2))      ) true
+     :else false
+
+    )
+    )
+  )
+)
 
 (defn title-length [book]
-  :-)
+
+  (count (:title book))
+
+  )
 
 (defn author-count [book]
-  :-)
+
+  (count (:authors book))
+
+  )
 
 (defn multiple-authors? [book]
-  :-)
+
+(if (< 1 (author-count book)) true false)
+
+)
 
 (defn add-author [book new-author]
-  :-)
+
+  (let [new (conj (:authors book) new-author)]
+    (assoc book :authors new)
+
+
+   )
+
+  )
 
 (defn alive? [author]
-  :-)
+(if (contains? author :death-year) false true)
+  )
 
 (defn element-lengths [collection]
   :-)
