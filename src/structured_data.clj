0c14eff016248145b125b22bf90e67af82cf0136
Daniel Pañeda <dpaneda@gmail.com>
2015-07-20 22:23:53 +0200

    Some fixes

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -20,16 +20,16 @@
   [bottom-left top-right])
 
 (defn width [[[x1 y1] [x2 y2]]]
-  (- y2 y1))
+  (- x2 x1))
 
 (defn height [[[x1 y1] [x2 y2]]]
-  (- x2 x1))
+  (- y2 y1))
 
 (defn square? [rectangle]
   (= (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
-  (/ (+ (width rectangle) (height rectangle)) 2))
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [[[x1 y1] [x2 y2]] [xp yp]]
   (and
@@ -42,7 +42,7 @@
     (contains-point? outer p2)))
 
 (defn title-length [book]
-  (count (:name book)))
+  (count (:title book)))
 
 (defn author-count [book]
   (count (:authors book)))
@@ -51,7 +51,7 @@
   (> (author-count book) 1))
 
 (defn add-author [book new-author]
-  (assoc book :authors (conj (:authors book new-author))))
+  (assoc book :authors (conj (:authors book) new-author)))
 
 (defn alive? [author]
   (not (contains? author :death-year)))
@@ -66,7 +66,9 @@
   (map :title books))
 
 (defn monotonic? [a-seq]
-  (apply <= a-seq))
+  (or 
+    (apply <= a-seq)
+    (apply >= a-seq)))
 
 (defn stars [n]
   (apply str (repeat n \*)))
@@ -76,10 +78,10 @@
     (op a-set elem)))
 
 (defn contains-duplicates? [a-seq]
-  (= (count a-set) (count (set a-seq))))
+  (> (count a-seq) (count (set a-seq))))
 
 (defn old-book->new-book [book]
-  (assoc book :authors (set :authors book)))
+  (assoc book :authors (set (:authors book))))
 
 (defn has-author? [book author]
   (contains? (:authors book) author))
@@ -88,14 +90,14 @@
   (apply clojure.set/union (map :authors books)))
 
 (defn all-author-names [books]
-  (map :name (authors books)))
+  (set (map :name (authors books))))
 
 (defn author->string [author]
   (let [years 
-        (if (contains? :birth-year author)
+        (if (contains? author :birth-year)
           (format " (%s - %s)" 
                   (:birth-year author) 
-                  (if (contains? :death-year author) (:death-year author) ""))
+                  (if (contains? author :death-year) (:death-year author) ""))
           "")]
     (str (:name author) years)))
 
@@ -103,13 +105,13 @@
   (apply str (interpose ", " (map author->string authors))))
 
 (defn book->string [book]
-  (str (:name book) ", written by " (authors->string (:authors book))))
+  (str (:title book) ", written by " (authors->string (:authors book))))
 
 (defn books->string [books]
   (let [header 
         (cond
-          (empty? books "No books.")
-          (= 1 (count book)) "1 book."
+          (empty? books) "No books."
+          (= 1 (count books)) "1 book."
           :else (str (count books) " books."))]
     (if (empty? books) 
       header
@@ -125,7 +127,7 @@
   (filter alive? authors))
 
 (defn has-a-living-author? [book]
-  (not (empty (filter alive? (:authors book)))))
+  (not (empty? (filter alive? (:authors book)))))
 
 (defn books-by-living-authors [books]
   (filter has-a-living-author? books))
