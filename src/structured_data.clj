b0de2afefbc2a7704999da9ded764353fcf802f5
HeikkiR <heikki.j.rantala@helsinki.fi>
2015-08-26 12:58:33 +0300

    puolet tehty

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,17 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [tupla (+ x x)] (Math/pow tupla tupla)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z] v]
+  (+ x z)))
 
 (defn point [x y]
   [x y])
@@ -19,40 +20,50 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (defn abs [x]
+      (if (< x 0) (- x) x))
+         (abs (- x1 x2))))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (defn abs [x]
+      (if (< x 0) (- x) x))
+         (abs (- y1 y2))))
 
 (defn square? [rectangle]
-  :-)
+  (if (= (width rectangle) (height rectangle)) true false))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (let [[x y] point]
+      (if (and (<= x1 x x2) (<= y1 y y2)) true false))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] outer]
+    (let [[[x3 y3] [x4 y4]] inner]
+      (if (and (contains-point? outer [x3 y3]) (contains-point? outer [x4 y4])) true false))))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (if (>= (author-count book) 2) true false))
 
 (defn add-author [book new-author]
-  :-)
+  (assoc book :authors (conj (:authors book) new-author)))
 
 (defn alive? [author]
-  :-)
+  (if (contains? author :death-year) false true))
 
 (defn element-lengths [collection]
-  :-)
+  (map count collection))
 
 (defn second-elements [collection]
   :-)
@@ -111,4 +122,4 @@
 (defn books-by-living-authors [books]
   :-)
 
-; %________%
+(do-a-thing 7)
