0e0ad4d4900d7d3902bec562195beaba62ca1d3b
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2015-10-09 14:53:09 +0530

    finised third chapter. Data structure

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -55,24 +55,6 @@
   (let [[plower pupper] inner]
     (and (contains-point? outer plower) (contains-point? outer pupper))))
 
-
-(def book {:title "The city and the city"
-           :authors [{:name "China Miéville" :birth-year 1972}]})
-
-(def china {:name "China Miéville"  :birth-year 1972})
-(def octavia {:name "Octavia E. Butler"
-              :birth-year 1947
-              :death-year 2006})
-(def friedman {:name "Daniel Friedman" :birth-year 1944})
-(def felleisen {:name "Matthias Felleisen"})
-(def cities {:title "The city and the city" :authors [china]})
-(def wild-seed {:title "Wild Seed" :authors [octavia]})
-(def embassytown {:title "Embassytown" :authors [china]})
-(def little-schemer {:title "The Little Schemer"
-                     :authors [friedman felleisen]})
-
-
-
 (defn title-length [book]
   (count (:title book)))
 
@@ -112,43 +94,85 @@
 (defn contains-duplicates? [a-seq]
   (not= (count a-seq) (count (set a-seq))))
 
+;; (def china {:name "China Miéville", :birth-year 1972})
+;; (def octavia {:name "Octavia E. Butler"
+;;               :birth-year 1947
+;;               :death-year 2006})
+;; (def friedman {:name "Daniel Friedman" :birth-year 1944})
+;; (def felleisen {:name "Matthias Felleisen"})
+
+;; (def cities {:title "The City and the City" :authors #{china}})
+;; (def wild-seed {:title "Wild Seed", :authors #{octavia}})
+;; (def embassytown {:title "Embassytown", :authors #{china}})
+;; (def little-schemer {:title "The Little Schemer"
+;;                      :authors #{friedman, felleisen}})
+
+;; (def books [cities, wild-seed, embassytown, little-schemer])
+
 (defn old-book->new-book [book]
-  :-)
+  (assoc book :authors (set (:authors book))))
 
 (defn has-author? [book author]
-  :-)
+  (contains? (:authors book) author))
 
 (defn authors [books]
-  :-)
+  (set (apply clojure.set/union
+              (map :authors books))))
 
 (defn all-author-names [books]
-  :-)
+  (set (map :name (authors books))))
 
 (defn author->string [author]
-  :-)
+  (str (:name author)
+       (if (nil? (:birth-year author))
+         ""
+         (str " (" (str (:birth-year author))
+              " - " (str (:death-year author)) ")"))))
 
 (defn authors->string [authors]
-  :-)
+  (apply str (interpose ", " (map author->string authors))))
 
 (defn book->string [book]
-  :-)
+  (str (:title book) ", written by "
+       (authors->string (:authors book))))
 
 (defn books->string [books]
-  :-)
+  (let [final-string (str (apply str (interpose ". " (map book->string books))) ".")
+        num-book (count books)]
+    (cond
+      (= 0 num-book) "No books."
+      (= 1 num-book) (str "1 book. " (str final-string))
+      :else (str (str num-book) " books. " (str final-string)))))
 
 (defn books-by-author [author books]
-  :-)
+  (filter (fn [x] (has-author? x author)) books))
 
 (defn author-by-name [name authors]
-  :-)
+  (let [lst (filter (fn [x] (= name (:name x))) authors)]
+    (cond
+      (empty? lst) nil
+      :else (first lst))))
+
+;;(def authors #{china, felleisen, octavia, friedman})
 
 (defn living-authors [authors]
-  :-)
+  (filter (fn [x] (nil? (:death-year x))) authors))
+
+;; (def jrrtolkien {:name "J. R. R. Tolkien" :birth-year 1892 :death-year 1973})
+;; (def christopher {:name "Christopher Tolkien" :birth-year 1924})
+;; (def kay {:name "Guy Gavriel Kay" :birth-year 1954})
+
+;; (def silmarillion {:title "Silmarillion"
+;;                    :authors #{jrrtolkien, christopher, kay}})
+;; (def dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
+;; (def zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
+
+;; (def deus-irae {:title "Deus Irae", :authors #{dick, zelazny}})
 
 (defn has-a-living-author? [book]
-  :-)
+  (not (empty? (living-authors (:authors book)))))
 
 (defn books-by-living-authors [books]
-  :-)
+  (filter has-a-living-author? books))
 
 ; %________%
