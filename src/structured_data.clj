b6ca5766840b8caa71a73fba4cc5ecdf15bb2a33
dm1299 <dm1299onion@gmail.com>
2015-10-23 18:05:31 +0800

    10/23/2015

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,19 +1,24 @@
 (ns structured-data)
 
+; exercise-1
 (defn do-a-thing [x]
   (let [y (+ x x)]
   (Math/pow y y)))
 
+; exercise-2
 (defn spiff [v]
   (+ (get v 0) (get v 2)))
 
+; exercise-3
 (defn cutify [v]
   (conj v "<3"))
 
+; exercise-4
 (defn spiff-destructuring [v]
   (let [[x y z] v]
     (+ x z)))
 
+; exercise-5
 (defn point [x y]
   [x y])
 
@@ -28,18 +33,30 @@
   (let [[[x0 y0] [x1 y1]] rectangle]
     (- y1 y0)))
 
+; exercise-6
 (defn square? [rectangle]
-  :-)
+  (let [[[x0 y0] [x1 y1]] rectangle]
+    (== (- x0 x1) (- y0 y1))))
 
+; exercise-7
 (defn area [rectangle]
-  :-)
+  (let [[[x0 y0] [x1 y1]] rectangle]
+      (* (- x0 x1) (- y0 y1))))
 
+; exercise-8
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x0 y0] [x1 y1]] rectangle
+        [x y] point]
+    (and (<= x0 x x1) 
+         (<= y0 y y1))))
 
+; exercise-9
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[bottom-left top-right] inner]
+       (and (contains-point? outer bottom-left) 
+            (contains-point? outer top-right))))
 
+; exercise-10
 (defn title-length [book]
   :-)
 
