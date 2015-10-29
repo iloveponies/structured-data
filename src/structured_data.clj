f0892df59493dcfd9a23a9702519808746e303c7
Miika Säynevirta <mayse@iki.fi>
2015-10-29 12:56:32 +0200

    13 ex

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -70,27 +70,29 @@
   )
 
 (defn contains-rectangle? [outer inner]
-
-  
   (let [[[x1 y1][x2 y2]] inner]
-    (and(contains-point? outer [x1 y1])(contains-point? outer [x2 y2]))
-
-  )
-    
-  
-)
+    (and (contains-point? outer [x1 y1])
+         (contains-point? outer [x2 y2]))))
 
 (defn title-length [book]
-  :-)
+  
+  (count (:title book))
+  
+  )
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (if(< 1 (author-count book)) true
+        false))
 
 (defn add-author [book new-author]
-  :-)
+    
+  (let [new-authors (conj (:authors book) new-author)]
+    (assoc book :authors new-authors))
+  
+)  
 
 (defn alive? [author]
   :-)
