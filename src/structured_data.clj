103efc49024dcefb1dbd32dd0df15b6d357ae7fc
Otso Björklund <otso.bjorklund@gmail.com>
2015-11-07 17:34:02 +0200

    Structured data exercises: 1–9

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,20 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+    (let [xx (+ x x)]
+      (Math/pow xx xx)
+      ))
 
 (defn spiff [v]
-  :-)
+  ( + (get v 0) (get v 2) ))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3") )
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[a b c] v ]
+    ( + a c ) )
+)
 
 (defn point [x y]
   [x y])
@@ -18,23 +22,44 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
+(defn distance [a b]
+  (if ( > (- a b) 0 )
+    (- a b)
+    ( * -1 (- a b) ) ))
+
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle ]
+    (distance x1 x2) )
+)
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle ]
+    (distance y1 y2)
+  )
+)
 
 (defn square? [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle ]
+    (if ( == (distance x1 x2) (distance y1 y2) ) true false )
+  ))
 
 (defn area [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle ]
+   (* (distance x1 x2) (distance y1 y2) )
+  )
+)
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle ]
+    (let [[px py] point ]
+      (if (and ( <= x1 px x2 ) ( <= y1 py y2 )) true false) ))
+)
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[p1 p2] inner]
+    (if (and (contains-point? outer p1) (contains-point? outer p2) ) true false)
+  )
+)
 
 (defn title-length [book]
   :-)
