6c03842eb3c2da282cb6a46f137cdb118d9faa6b
Otso Björklund <otso.bjorklund@gmail.com>
2015-11-12 19:11:40 +0200

    Exercises 10-14

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -61,20 +61,38 @@
   )
 )
 
+
+; Todo: For own testing only, remove when ready
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def friedman {:name "Daniel Friedman" :birth-year 1944})
+(def felleisen {:name "Matthias Felleisen"})
+
+(def cities {:title "The City and the City" :authors [china]})
+(def wild-seed {:title "Wild Seed", :authors [octavia]})
+(def embassytown {:title "Embassytown", :authors [china]})
+(def little-schemer {:title "The Little Schemer"
+                     :authors [friedman, felleisen]})
+; ------------------------------------------
+
+
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (if ( > (author-count book) 1 ) true false ))
 
 (defn add-author [book new-author]
-  :-)
+  (let [newAuthors (conj (:authors book) new-author)]
+    (assoc book :authors newAuthors)))
 
 (defn alive? [author]
-  :-)
+  (if (:death-year author) false true))
 
 (defn element-lengths [collection]
   :-)
