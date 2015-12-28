272bd59478e63d51a7cc84c25ad1d8a263547d8e
Juuso Hyvönen <juuso.hyvonen@solita.fi>
2015-12-28 09:42:15 +0200

    Ettei mene hukkaan.

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+    (Math/pow xx xx)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[a b c] v]
+    (+ a c)))
 
 (defn point [x y]
   [x y])
@@ -19,67 +21,96 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (if (= (- x1 x2) (- y1 y2))
+      true
+      false)))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
+
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [xpoint ypoint] point]
+    (and (<= x1 xpoint x2) (<= y1 ypoint y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[x y] inner]
+    (and (contains-point? outer x) (contains-point? outer y))))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (count (:authors book)) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (assoc book :authors (conj (:authors book) new-author)))
 
 (defn alive? [author]
-  :-)
+  (nil? (:death-year author)))
+
 
 (defn element-lengths [collection]
-  :-)
+  (map count collection))
 
 (defn second-elements [collection]
-  :-)
+  (let [second (fn [x] (get x 1))]
+    (map second collection)))
 
 (defn titles [books]
-  :-)
-
-(defn monotonic? [a-seq]
-  :-)
+  (map :title books))
 
 (defn stars [n]
-  :-)
+  (apply str (repeat n \*)))
+
+(defn monotonic? [a-seq]
+  (or (apply <= a-seq) (apply >= a-seq)))
 
 (defn toggle [a-set elem]
-  :-)
+  (if (contains? a-set elem)
+    (disj a-set elem)
+    (conj a-set elem)))
+
+; (defn contains-duplicates? [a-seq]
+;   (let [[a & rest] a-seq]
+;     (if (contains? rest a)
+;       false
+;       (contains-duplicates? rest))))
+
+; defn contains-duplicates? [a-seq]
+; (not= (count (distinct a-seq)) (count a-seq)))
 
 (defn contains-duplicates? [a-seq]
-  :-)
+  (not= (count (set a-seq)) (count a-seq)))
 
 (defn old-book->new-book [book]
-  :-)
+  (assoc book :authors (set (:authors book))))
 
 (defn has-author? [book author]
-  :-)
+  (contains? (:authors book) author))
+
+
+
+;(defn all-author-names [books]
+;  (let [author-names
+;        (fn [book] (map :name (:authors book)))]
+;(set (apply concat (map author-names books)))))
 
 (defn authors [books]
-  :-)
+    (set (apply concat (map :author books))))
 
 (defn all-author-names [books]
   :-)
