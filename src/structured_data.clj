13ed68870cfa4b71da4b486918e22f0275cdf2b0
Dante Serenelli <Dante Serenelli>
2016-03-03 02:10:05 -0300

    partial commit

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,24 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [a (+ x x)]
+    (Math/pow a a)
+    )
+  )
 
 (defn spiff [v]
-  :-)
+  (+ (first v) (get v 2))
+  )
 
 (defn cutify [v]
-  :-)
+  (conj v "<3")
+  )
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[a b c] [(first v) :q (get v 2)]]
+    (+ a c)
+    )
+  )
 
 (defn point [x y]
   [x y])
@@ -19,22 +27,40 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)
+    )
+  )
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)
+    )
+  )
 
 (defn square? [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (= (- x2 x1) (- y2 y1))
+    )
+  )
 
 (defn area [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (* (- y2 y1) (- x2 x1)))
+    )
 
-(defn contains-point? [rectangle point]
-  :-)
+(defn contains-point? [rectanglex point]
+  (let [[[x1 y1] [x2 y2]] rectanglex [x3 y3] point]
+    (and (<= x1 x3 x2) (<= y1 y3 y2)))
+  )
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] outer [[x3 y3] [x4 y4]] inner]
+    (and
+      (contains-point? (rectangle [x1 y1] [x2 y2]) (point x3 y3))
+      (contains-point? (rectangle [x1 y1] [x2 y2]) (point x4 y4)))
+  ))
+
 
 (defn title-length [book]
   :-)
