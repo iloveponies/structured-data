af825ac6194242d4dfd01cac8ec101007ea5cf3a
Aaron Hoffer <github@hoffer.org>
2016-03-16 23:50:00 -0700

    WIP

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -94,8 +94,8 @@ Math/PI
 
 (defn point-less-than? [point1 point2]
     (and
-        (< (x point1) (x point2))
-        (< (y point1) (y point2))))
+        (<= (x point1) (x point2))
+        (<= (y point1) (y point2))))
 
   (point-less-than? [2 1] [2 2])
 
@@ -108,22 +108,67 @@ Math/PI
 
 
 (defn contains-rectangle? [outer inner]
-    :-)
+ (and  (contains-point? outer (ll inner))
+       (contains-point? outer (ur inner))))
+
+
+(contains-rectangle? (rectangle [0 0] [3 3])
+                     (rectangle [1 1] [2 2]))
+(contains-rectangle? (rectangle [0 0] [2 2])
+                     (rectangle [1 1] [3 3]))
+(contains-rectangle? (rectangle [0 0] [1 1])
+                     (rectangle [0 0] [1 1]))
+(contains-rectangle? (rectangle [0 0] [1 1])
+                     (rectangle [1 1] [2 2]))
+
+
 
 (defn title-length [book]
-    :-)
+    (count
+        (:title book)))
+
+
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def friedman {:name "Daniel Friedman" :birth-year 1944})
+(def felleisen {:name "Matthias Felleisen"})
+
+(def cities {:title "The City and the City" :authors [china]})
+(def wild-seed {:title "Wild Seed", :authors [octavia]})
+(def embassytown {:title "Embassytown", :authors [china]})
+(def little-schemer {:title "The Little Schemer"
+                     :authors [friedman, felleisen]})
+
+(title-length cities)         ;=> 21
 
 (defn author-count [book]
-    :-)
+    (count
+        (:authors book)))
+
 
 (defn multiple-authors? [book]
-    :-)
+    (> (author-count book) 0))
 
 (defn add-author [book new-author]
-    :-)
+    (let [ new-authors  (conj (:authors book) new-author) ]
+        (assoc book :authors new-authors)))
+
+
+(contains? {"a" 1} "a")   ;=> true
+(contains? {"a" 1} 1)     ;=> false
+(contains? {"a" nil} "a") ;=> true
+(contains? cities :title) ;=> true
+(contains? cities :name)  ;=> false
+
 
 (defn alive? [author]
-    :-)
+    (not
+        (contains? author :death-year)))
+
+(alive? china)   ;=> true
+(alive? octavia) ;=> false
 
 (defn element-lengths [collection]
     :-)
