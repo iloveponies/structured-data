9ad7f28db146d472c904eb3809f3719f250546da
Philippus <philippus@gmail.com>
2016-03-22 22:33:14 +0100

    defined the functions up til exercise 12

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,22 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+    (Math/pow xx xx)))
+
 
 (defn spiff [v]
-  :-)
+  (cond
+    (empty? v) 0
+    (<= (count v) 2) (get v 0)
+    :else (+ (get v 0) (get v 2))))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z] v]
+    (+ x z)))
 
 (defn point [x y]
   [x y])
@@ -19,31 +25,43 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (cond
+      (= x1 x2) 0
+      (> x1 x2) (- x1 x2)
+      :else (- x2 x1))))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (cond
+      (= y1 y2) 0
+      (> y1 y2) (- y1 y2)
+      :else (- y2 y1))))
 
 (defn square? [rectangle]
-  :-)
+  (= (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [a1 b1] point]
+    (and (<= x1 a1 x2) (<= y1 b1 y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[pointa pointb] inner]
+    (and (contains-point? outer pointa) (contains-point? outer pointb))
+  ))
 
 (defn title-length [book]
-  :-)
+  (count (get book :title)))
 
 (defn author-count [book]
-  :-)
+  (count (get book :authors)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
 (defn add-author [book new-author]
   :-)
