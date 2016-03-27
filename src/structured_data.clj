dc2cad580b1019ddb567d6f8739f23352774cab7
Gyorgy Farkas <gyfarkas@gmail.com>
2016-03-27 22:52:31 +0200

    solved

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [double (+ x x)]
+    (Math/pow double double))
+)
 
 (defn spiff [v]
-  :-)
+  (+ (first v) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
-(defn spiff-destructuring [v]
-  :-)
+(defn spiff-destructuring [[x _ y & v]]
+  (+ x y))
 
 (defn point [x y]
   [x y])
@@ -18,97 +20,122 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
+(defn width [[[x1 y1] [x2 y2]]]
+  (Math/abs (- x1 x2)))
 
-(defn height [rectangle]
-  :-)
+(defn height [[[x1 y1] [x2 y2]]]
+  (Math/abs (- y1 y2)))
 
 (defn square? [rectangle]
-  :-)
+  (= (width rectangle)(height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [p1 p2] point]
+    (and (<= x1 p1 x2) (<= y1 p2 y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] outer
+        [[ix1 iy1] [ix2 iy2]] inner]
+    (and (<= x1 ix1 ix2 x2) (<= y1 iy1 iy2 y2))))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (< 1 (author-count book)))
 
 (defn add-author [book new-author]
-  :-)
+  (assoc book :authors (conj (:authors book) new-author)))
 
 (defn alive? [author]
-  :-)
+  (nil? (:death-year author)))
 
 (defn element-lengths [collection]
-  :-)
+  (map count collection))
 
 (defn second-elements [collection]
-  :-)
+  (map #(get % 1) collection))
 
 (defn titles [books]
-  :-)
+  (concat  (map #(:title %) books)))
 
 (defn monotonic? [a-seq]
-  :-)
+  (or (apply >= a-seq) (apply <= a-seq)))
 
 (defn stars [n]
-  :-)
+  (apply str (repeat n "*")))
 
 (defn toggle [a-set elem]
-  :-)
+  (if (contains? a-set elem)
+    (disj a-set elem)
+    (conj a-set elem)))
+
 
 (defn contains-duplicates? [a-seq]
-  :-)
+  (not  (= (count a-seq) (count (set a-seq)))))
 
 (defn old-book->new-book [book]
-  :-)
+  (assoc book :authors (set (:authors book))))
 
 (defn has-author? [book author]
-  :-)
+  (contains? (:authors book) author))
 
 (defn authors [books]
-  :-)
+  (set (apply concat (map :authors books))))
 
 (defn all-author-names [books]
-  :-)
+  (set  (map :name (authors books))))
 
 (defn author->string [author]
-  :-)
+  (apply str
+         (list
+          (:name author)
+          (if (nil? (:birth-year author))
+            ""
+            (apply str " (" (:birth-year author) " - " (:death-year author) ")"))
+          )))
 
 (defn authors->string [authors]
-  :-)
+  (apply str  (interpose ", " (map author->string authors))))
 
 (defn book->string [book]
-  :-)
+  (apply str
+         (:title book)
+         ", written by "
+         (authors->string (:authors book)))
+  )
 
 (defn books->string [books]
-  :-)
+  (let [ c (count books)
+        cstr (if (> c 1) " books. " " book. ")
+        ]
+    (if (= c 0)
+      "No books."
+      (str c cstr (apply str (interpose ". " (map book->string books))) ".")
+    )
+   )
+  )
 
 (defn books-by-author [author books]
-  :-)
+  (filter #(has-author? %  author) books))
 
 (defn author-by-name [name authors]
-  :-)
+  (first  (filter #(= (:name %) name) authors)))
 
 (defn living-authors [authors]
-  :-)
+  (filter alive? authors))
 
 (defn has-a-living-author? [book]
-  :-)
+  (not  (empty? (living-authors (:authors book)))))
 
 (defn books-by-living-authors [books]
-  :-)
+  (filter has-a-living-author? books))
 
 ; %________%
