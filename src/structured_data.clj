e9ecff557e5edb847e2fd13cb663ed67c12bddcf
Peter Laakso <peter.laakso@gmail.com>
2016-05-25 19:19:12 +0300

    structured-data 1-3

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,13 +1,14 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xpx (+ x x)]
+    (Math/pow xpx xpx)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
   :-)
