af093e4cb8e92c937fa8319ad8f03125fa4e58b4
Peter Laakso <peter.laakso@gmail.com>
2016-06-03 15:17:30 +0300

    structured-data 31

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -129,8 +129,7 @@
    (filter (fn [book] (has-author? book author)) books))
 
 (defn author-by-name [name authors]
-  :-)
-
+  (first (filter (fn [author] (= name (:name author))) authors)))
 (defn living-authors [authors]
   :-)
 
