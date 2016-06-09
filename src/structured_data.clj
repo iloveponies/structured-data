6ebda24f2f9d42a544cb251aa93cb1818609d885
Juho Leinonen <juholei@gmail.com>
2016-06-09 19:16:09 +0300

    A couple more exercises done

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -34,10 +34,14 @@
   (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [px py] point]
+    (and (<= x1 px x2) (<= y1 py y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[p1 p2] inner]
+    (and (contains-point? outer p1) 
+         (contains-point? outer p2))))
 
 (defn title-length [book]
   :-)
