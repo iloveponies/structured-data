d540c5dbf69b0bf47c14c17a458f4dc68b4b554a
Tiera <tiera.hirvonen@gmail.com>
2016-06-17 18:42:56 +0300

    asda

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,22 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (Math/pow (+ x x) (+ x x)))
 
 (defn spiff [v]
-  :-)
+  (if (and (= (get v 0) nil) (= (get v 2) nil))
+    "?"
+    (+ (get v 2) (get v 0))))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (if (and (= (get v 0) nil) (= (get v 2) nil))
+    "?"
+    (let [[a b c] v]
+    (+ a c))))
+
 
 (defn point [x y]
   [x y])
@@ -19,34 +25,66 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (if (> x1 x2)
+      (- x1 x2)
+      (- x2 x1))))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (if (> y1 y2)
+      (- y1 y2)
+      (- y2 y1))))
 
 (defn square? [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (if (> y1 y2)
+      (if (> x1 x2)
+      (= (- x1 x2) (- y1 y2))
+      (= (- x2 x1) ((- y2 y1))))
+      (if (> x1 x2)
+      (= (- x1 x2) (- y1 y2))
+      (= (- x2 x1) (- y2 y1))))))
+
 
 (defn area [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (if (> y1 y2)
+      (if (> x1 x2)
+      (* (- x1 x2) (- y1 y2))
+      (* (- x2 x1) ((- y2 y1))))
+      (if (> x1 x2)
+      (* (- x1 x2) (- y1 y2))
+      (* (- x2 x1) (- y2 y1))))))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (let [[x3 y3] point]
+      (and (>= x1 x3) (<= x2 x3) (>= y1 y3) (<= y2 y3)))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] outer]
+    (let [[[x3 y3] [x4 y4]] inner]
+      (and (contains-point? (rectangle [x1 y1] [x2 y2])
+                 (point x3 y3)) (contains-point? (rectangle [x1 y1] [x2 y2])
+                 (point x4 y4))))))
+
 
 (defn title-length [book]
-  :-)
+  (count (get book :title)))
 
 (defn author-count [book]
-  :-)
+(count (get book :authors)))
 
 (defn multiple-authors? [book]
-  :-)
+  (< 1 (count (get book :authors))))
 
 (defn add-author [book new-author]
-  :-)
+  (let [original book
+      new      (assoc original :authors new-author)]
+  (conj (assoc original :title nil) new-author)))
+
+
 
 (defn alive? [author]
   :-)
@@ -112,3 +150,4 @@
   :-)
 
 ; %________%
+
