2c89ad0d73de109172965798060d35f9b3db2528
Stanislav Skalický <skalicky.s@gmail.com>
2016-06-24 09:52:09 +0200

    excercise 1-13

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,28 @@
 (ns structured-data)
 
-(defn do-a-thing [x]
-  :-)
+(defn do-a-thing
+  "(defn do-a-thing [x]
+  (Math/pow (+ x x) (+ x x)))"
+  [x]
+  (let [xx (+ x x)]
+    (Math/pow xx xx))
+  )
 
-(defn spiff [v]
-  :-)
+(defn spiff
+  "takes a vector and returns the sum of the first and third elements of the vector"
+  [v]
+  (+ (get v 0) (get v 2)))
 
-(defn cutify [v]
-  :-)
+(defn cutify
+  " that takes a vector as a parameter and adds \"<3\" to its end"
+  [v]
+  (conj v "<3"))
 
-(defn spiff-destructuring [v]
-  :-)
+(defn spiff-destructuring
+  "Rewrite our earlier function spiff by destructuring its parameter. Call this new function"
+  [v]
+  (let [[first second third] v]
+    (+ first third)))
 
 (defn point [x y]
   [x y])
@@ -18,35 +30,55 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
+(defn width
+  [[[x1 y1] [x2 y2]]]
+  (- x2 x1))
 
-(defn height [rectangle]
-  :-)
+(defn height
+  [[[x1 y1] [x2 y2]]]
+  (- y2 y1))
 
-(defn square? [rectangle]
-  :-)
+(defn square?
+  "that returns true if rectangle is a square and otherwise false."
+  [[[x1 y1] [x2 y2]]]
+  (== (- x2 x1) (- y2 y1)))
 
-(defn area [rectangle]
-  :-)
+(defn area
+  "returns the area of the given rectangle."
+  [[[x1 y1] [x2 y2]]]
+  (* (- x2 x1) (- y2 y1))
+  )
 
-(defn contains-point? [rectangle point]
-  :-)
+(defn contains-point?
+  " returns true if rectangle contains point and otherwise false."
+  [[[x1 y1] [x2 y2]] [x y]]
+  (and (<= x1 x x2) (<= y1 y y2)))
 
-(defn contains-rectangle? [outer inner]
-  :-)
+(defn contains-rectangle?
+  "returns true if the rectangle inner is inside the rectangle outer and otherwise false."
+  [outer [x y]]
+  (and (contains-point? outer x) (contains-point? outer y)))
 
-(defn title-length [book]
-  :-)
+(defn title-length
+  "counts the length of the book’s title."
+  [book]
+  (count (:title book)))
 
-(defn author-count [book]
-  :-)
+(defn author-count
+  "that returns the amount of authors that book has."
+  [book]
+  (count (:authors book)))
 
-(defn multiple-authors? [book]
-  :-)
+(defn multiple-authors?
+  "returns true if book has multiple authors, otherwise false."
+  [book]
+  (> (author-count book) 1))
 
-(defn add-author [book new-author]
-  :-)
+(defn add-author
+  "takes a book and an author as a parameter and adds author to books authors."
+  [book new-author]
+  (let [authors (:authors book)]
+     (assoc book :authors (conj authors new-author))))
 
 (defn alive? [author]
   :-)
