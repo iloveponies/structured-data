bbe1aec4cff3fc86527cd256622408c8cce9f425
UMTti <toivanenpihla@gmail.com>
2016-07-07 19:26:33 +0300

    kaikki

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,14 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [plussa (+ x x)]
+    (Math/pow plussa plussa)))
 
-(defn spiff [v]
-  :-)
+(defn spiff [v] (+ (get v 0) (get v 2)))
 
-(defn cutify [v]
-  :-)
+(defn cutify [v] (conj v "<3"))
 
-(defn spiff-destructuring [v]
-  :-)
+(defn spiff-destructuring [v] (let [[a b c] v] (+ a c)))
 
 (defn point [x y]
   [x y])
@@ -18,53 +16,57 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
+(defn width [rectangle] (
+        let [eka (get rectangle 0) toka (get rectangle 1)]
+          (max (- (first eka) (first toka)) (- (- (first eka) (first toka))) )
+                          ))
 
-(defn height [rectangle]
-  :-)
+(defn height [rectangle] (
+  let [eka (get rectangle 0) toka (get rectangle 1)]
+          (max (- (second eka) (second toka)) (- (- (second eka) (second toka))) )
+                          ))
 
-(defn square? [rectangle]
-  :-)
+(defn square? [rectangle] (if (= (height rectangle) (width rectangle)) true false))
 
-(defn area [rectangle]
-  :-)
+(defn area [rectangle] (* (height rectangle) (width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [eka (get rectangle 0) toka (get rectangle 1)]
+    (if (and (<= (first eka) (first point) (first toka)) (<= (second eka) (second point) (second toka)) ) true false
+      )  ))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (if (and (contains-point? outer (first inner)) (contains-point? outer (second inner))  ) true false))
 
-(defn title-length [book]
-  :-)
+(defn title-length [book] (count (:title book)))
 
-(defn author-count [book]
-  :-)
+(defn author-count [book] (count (:authors book)))
 
-(defn multiple-authors? [book]
-  :-)
+(defn multiple-authors? [book] (if (> (author-count book) 1) true false))
 
-(defn add-author [book new-author]
-  :-)
+(defn add-author [book new-author] (assoc book :authors (conj (:authors book) new-author)))
 
-(defn alive? [author]
-  :-)
+(defn alive? [author] (if (= (:death-year author) nil) true false ))
 
-(defn element-lengths [collection]
-  :-)
+(defn element-lengths [collection] (map count collection))
 
-(defn second-elements [collection]
-  :-)
+(defn second-elements [collection] (map second collection))
 
-(defn titles [books]
-  :-)
+(defn titles [books] (map :title books))
 
-(defn monotonic? [a-seq]
-  :-)
 
-(defn stars [n]
-  :-)
+(defn increasing [x y]
+  (if (or (<= x y) (= y nil)) true false))
+
+(defn decreasing [x y]
+  (if (or (>= x y) (= y nil)) true false))
+
+(defn monotonic? [a-seq] (if
+            (or
+              (apply = (map increasing a-seq (rest a-seq))) (apply = (map decreasing a-seq (rest a-seq) )) ) true false ))
+
+
+(defn stars [n] (apply str (repeat n "*")))
 
 (defn toggle [a-set elem]
   :-)
