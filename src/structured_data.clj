9a067e4517757e99bab65573b1a06a98c309d5db
UMTti <toivanenpihla@gmail.com>
2016-07-07 23:26:39 +0300

    kaikki

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -68,32 +68,37 @@
 
 (defn stars [n] (apply str (repeat n "*")))
 
-(defn toggle [a-set elem]
-  :-)
+(defn toggle [a-set elem] (if (contains? a-set elem) (disj a-set elem) (conj a-set elem)))
 
-(defn contains-duplicates? [a-seq]
-  :-)
+(defn contains-duplicates? [a-seq] (if (= (count a-seq) (count (into #{} a-seq) )) false true) )
 
-(defn old-book->new-book [book]
-  :-)
+(defn old-book->new-book [book] (assoc book :authors (into #{} (:authors book)) ))
 
-(defn has-author? [book author]
-  :-)
+(defn has-author? [book author] (if (contains? (:authors book) author) true false ))
 
-(defn authors [books]
-  :-)
+(defn authors [books] (into #{} (apply concat (map :authors books))))
 
 (defn all-author-names [books]
-  :-)
+  (let [author-names
+         (fn [book] (map :name (:authors book)))]
+    (set (apply concat (map author-names books)))))
 
-(defn author->string [author]
-  :-)
+(defn author->string [author] (if (nil? (:birth-year author)) (str(:name author)) (str (:name author) " (" (:birth-year author) " - " (:death-year author) ")" ) ))
 
-(defn authors->string [authors]
-  :-)
+
+(defn author-names [authors]
+  (map :name authors))
+
+(defn add-pilkku [x y]
+  (if (= y nil) x (str x ", ") ))
+
+((defn authors->string [authors] (let [nimet (author-names authors)]
+
+  str (apply str (map add-pilkku nimet (rest nimet)  ) ) last nimet) )  [{:name "pihla"}, {:name "asd"}, {:name "asd1213"}] )
 
 (defn book->string [book]
   :-)
+
 
 (defn books->string [books]
   :-)
