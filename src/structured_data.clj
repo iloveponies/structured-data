da3ccdf086dc707a77c4b810192fb56241a7a13c
baobabKoodaa <baobabKoodaa@users.noreply.github.com>
2016-07-13 04:27:12 +0300

    kirjojen maarittelyt poistettu taas

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -100,17 +100,17 @@
 (defn old-book->new-book [book]
   (assoc book :authors (set (get book :authors))))
 
-(def china {:name "China Miéville", :birth-year 1972})
-(def octavia {:name "Octavia E. Butler"
-              :birth-year 1947
-              :death-year 2006})
-(def friedman {:name "Daniel Friedman" :birth-year 1944})
-(def felleisen {:name "Matthias Felleisen"})
-
-(def cities {:title "The City and the City" :authors #{china}})
-(def wild-seed {:title "Wild Seed", :authors #{octavia}})
-(def embassytown {:title "Embassytown", :authors #{china}})
-(def little-schemer {:title "The Little Schemer"
+;(def china {:name "China Miéville", :birth-year 1972})
+;(def octavia {:name "Octavia E. Butler"
+;              :birth-year 1947
+;              :death-year 2006})
+;(def friedman {:name "Daniel Friedman" :birth-year 1944})
+;(def felleisen {:name "Matthias Felleisen"})
+;
+;(def cities {:title "The City and the City" :authors #{china}})
+;(def wild-seed {:title "Wild Seed", :authors #{octavia}})
+;(def embassytown {:title "Embassytown", :authors #{china}})
+;(def little-schemer {:title "The Little Schemer"
                      :authors #{friedman, felleisen}})
 (def books [cities, wild-seed, embassytown, little-schemer])
 
