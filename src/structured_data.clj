665b1712de5484816246a796139a657e714c413f
baobabKoodaa <baobabKoodaa@users.noreply.github.com>
2016-07-13 04:37:58 +0300

    poistettu kirjamaarittelyt myos kommenteista

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -89,33 +89,6 @@
 
 
 
-(def china {:name "China Miéville", :birth-year 1972})
-(def octavia {:name "Octavia E. Butler"
-              :birth-year 1947
-              :death-year 2006})
-(def friedman {:name "Daniel Friedman" :birth-year 1944})
-(def felleisen {:name "Matthias Felleisen"})
-
-(def cities {:title "The City and the City" :authors [china]})
-(def wild-seed {:title "Wild Seed", :authors [octavia]})
-(def embassytown {:title "Embassytown", :authors [china]})
-(def little-schemer {:title "The Little Schemer"
-                     :authors [friedman, felleisen]})
-
-
-
-;(def china {:name "China Miéville", :birth-year 1972})
-;(def octavia {:name "Octavia E. Butler"
-;              :birth-year 1947
-;              :death-year 2006})
-;(def friedman {:name "Daniel Friedman" :birth-year 1944})
-;(def felleisen {:name "Matthias Felleisen"})
-;
-;(def cities {:title "The City and the City" :authors #{china}})
-;(def wild-seed {:title "Wild Seed", :authors #{octavia}})
-;(def embassytown {:title "Embassytown", :authors #{china}})
-;(def little-schemer {:title "The Little Schemer"
-;                     :authors #{friedman, felleisen}})
 
 
 
