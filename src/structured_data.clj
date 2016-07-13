a32b5f2118ad2b2e3bf3a26fcfe9428c424f6395
baobabKoodaa <baobabKoodaa@users.noreply.github.com>
2016-07-13 04:32:00 +0300

    vanhat kirjojen maarittelyt palautettu

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -44,19 +44,6 @@
   (let [[p1 p2] inner]
     (and (contains-point? outer p1) (contains-point? outer p2))))
 
-;(def china {:name "China Miéville", :birth-year 1972})
-;(def octavia {:name "Octavia E. Butler"
-;              :birth-year 1947
-;              :death-year 2006})
-;(def friedman {:name "Daniel Friedman" :birth-year 1944})
-;(def felleisen {:name "Matthias Felleisen"})
-;
-;(def cities {:title "The City and the City" :authors [china]})
-;(def wild-seed {:title "Wild Seed", :authors [octavia]})
-;(def embassytown {:title "Embassytown", :authors [china]})
-;(def little-schemer {:title "The Little Schemer"
-;                     :authors [friedman, felleisen]})
-
 (defn title-length [book]
   (count (:title book)))
 
@@ -100,6 +87,23 @@
 (defn old-book->new-book [book]
   (assoc book :authors (set (get book :authors))))
 
+
+
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def friedman {:name "Daniel Friedman" :birth-year 1944})
+(def felleisen {:name "Matthias Felleisen"})
+
+(def cities {:title "The City and the City" :authors [china]})
+(def wild-seed {:title "Wild Seed", :authors [octavia]})
+(def embassytown {:title "Embassytown", :authors [china]})
+(def little-schemer {:title "The Little Schemer"
+                     :authors [friedman, felleisen]})
+
+
+
 ;(def china {:name "China Miéville", :birth-year 1972})
 ;(def octavia {:name "Octavia E. Butler"
 ;              :birth-year 1947
@@ -111,7 +115,10 @@
 ;(def wild-seed {:title "Wild Seed", :authors #{octavia}})
 ;(def embassytown {:title "Embassytown", :authors #{china}})
 ;(def little-schemer {:title "The Little Schemer"
-                     :authors #{friedman, felleisen}})
+;                     :authors #{friedman, felleisen}})
+
+
+
 (def books [cities, wild-seed, embassytown, little-schemer])
 
 (defn has-author? [book author]
