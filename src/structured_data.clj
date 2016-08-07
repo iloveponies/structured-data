9ec24f53e3a22a4b6fc468d864b727857404c572
9illes <9illes@workstation.localdomain>
2016-08-07 15:24:39 +0200

    completed

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,23 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+    (Math/pow xx xx)))
 
 (defn spiff [v]
-  :-)
+  (cond
+    (< 2 (count v)) (+ (get v 0) (get v 2))
+    :else  "?"
+    ))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
-(defn spiff-destructuring [v]
-  :-)
+(defn spiff-destructuring [[a b c]]
+  (cond
+    (and a c) (+ a c)
+    :else "?"
+  ))
 
 (defn point [x y]
   [x y])
@@ -19,96 +26,129 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)
+    ))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)
+    ))
 
 (defn square? [rectangle]
-  :-)
+  (= (height rectangle) (width rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[x y] point
+        [[min-x min-y] [max-x max-y]] rectangle]
+    (and (<= min-x x max-x) (<= min-y y max-y))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[inner-bottom-left inner-top-right] inner]
+    (and (contains-point? outer inner-bottom-left)
+         (contains-point? outer inner-top-right))
+    ))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (let [authors (:authors book)]
+    (assoc book :authors (conj authors new-author))
+    ))
 
 (defn alive? [author]
-  :-)
+  (not (contains? author :death-year)))
 
 (defn element-lengths [collection]
-  :-)
+  (map count collection))
 
 (defn second-elements [collection]
-  :-)
+  (map (fn [s] (second s)) collection))
 
 (defn titles [books]
-  :-)
+  (map :title books))
 
 (defn monotonic? [a-seq]
-  :-)
+  (let [monotonic (fn [c s]
+             (apply c s))]
+    (or (monotonic >= a-seq) (monotonic <= a-seq))
+  ))
 
 (defn stars [n]
-  :-)
+  (apply str (repeat n "*")))
 
 (defn toggle [a-set elem]
-  :-)
+  (if (contains? a-set elem)
+    (disj a-set elem)
+    (conj a-set elem)))
 
 (defn contains-duplicates? [a-seq]
-  :-)
+  (not (= (count a-seq) (count (set a-seq)))))
 
 (defn old-book->new-book [book]
-  :-)
+  (let [authors (:authors book)
+        authors-set (set authors)]
+        (assoc book :authors authors-set)
+        ))
 
 (defn has-author? [book author]
-  :-)
+  (contains? (:authors book) author))
 
 (defn authors [books]
-  :-)
+  (apply clojure.set/union (map :authors books)))
 
 (defn all-author-names [books]
-  :-)
+  (set (map :name (authors books))))
 
 (defn author->string [author]
-  :-)
+  (let [name (:name author)
+        years (if (contains? author :birth-year)
+                (str " (" (:birth-year author) " - " (:death-year author) ")")
+                nil)]
+    (str name (or years ""))))
 
 (defn authors->string [authors]
-  :-)
+  (apply str (interpose ", " (map author->string authors))))
 
 (defn book->string [book]
-  :-)
+  (str (:title book) ", written by " (authors->string (:authors book))))
 
 (defn books->string [books]
-  :-)
+  (let [nb-book (count books)
+        nb-book-str (cond
+                  (= nb-book 0) "No books"
+                  (= nb-book 1) "1 book. "
+                  :else (str nb-book " books. "))
+        books-str (apply str (interpose ". " (map book->string books)))]
+        (str nb-book-str books-str ".")
+    ))
 
 (defn books-by-author [author books]
-  :-)
+  (filter (fn [book] (has-author? book author)) books))
 
 (defn author-by-name [name authors]
-  :-)
+  (first (filter (fn [author]
+            (= name (:name author))) authors)))
 
 (defn living-authors [authors]
-  :-)
+  (filter alive? authors))
 
 (defn has-a-living-author? [book]
-  :-)
+  (not (empty? (living-authors (:authors book)))))
+
 
 (defn books-by-living-authors [books]
-  :-)
+  (filter has-a-living-author? books))
+
 
 ; %________%
