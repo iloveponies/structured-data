0f128fe6930c858bc181f87290c20f2b097f22b7
Mikko <mikko.o.niemi@gmail.com>
2016-08-13 21:44:41 +0300

    Tehtävät 1 - 26

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -97,7 +97,7 @@
   (apply clojure.set/union (map :authors books)))
 
 (defn all-author-names [books]
-  (map :name (seq (authors1 books))))
+  (map :name (seq (authors books))))
 
 (defn author->string [author]
     (let [nimi (fn [author] (str (:name author)))
