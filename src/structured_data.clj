3920b43c9ff0a674de9d6a2292ffc3f9da603085
ninjyni <ninjyni@gmail.com>
2016-10-25 21:55:57 +0300

    exercises 1-9

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,26 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [x (+ x x)]
+  (Math/pow x x)))
 
 (defn spiff [v]
-  :-)
+  (if
+    (and (vector? v)
+         (>= (count v) 3))
+    (+ (get v 0) (get v 2))
+    nil))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[a b c] v]
+  (if
+    (and (vector? v)
+         (>= (count v) 3))
+    (+ a c)
+    nil)))
 
 (defn point [x y]
   [x y])
@@ -19,22 +29,36 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (if (== (height rectangle) (width rectangle))
+    true
+    false))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+       [xp yp] point]
+    (if (and (<= x1 xp x2)
+              (<= y1 yp y2))
+      true
+      false)))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1o y1o] [x2o y2o]] outer
+        [[x1i y1i] [x2i y2i]] inner]
+    (if (and (contains-point? outer [x1i y1i])
+             (contains-point? outer [x2i y2i]))
+      true
+      false)))
 
 (defn title-length [book]
   :-)
