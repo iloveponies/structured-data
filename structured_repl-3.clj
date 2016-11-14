f1fbd36ba2af14a5dd70f52f15204c936cac1636
tero.hakuli@iki.fi <tero.hakuli@iki.fi>
2016-11-14 20:14:39 +0000

    more exercises

---
+++ structured_repl-3.clj
@@ -0,0 +1,28 @@
+;; Anything you type in here will be executed
+;; immediately with the results shown on the
+;; right.
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def friedman {:name "Daniel Friedman" :birth-year 1944})
+(def felleisen {:name "Matthias Felleisen"})
+
+(def cities {:title "The City and the City" :authors #{china}})
+(def wild-seed {:title "Wild Seed", :authors #{octavia}})
+(def embassytown {:title "Embassytown", :authors #{china}})
+(def little-schemer {:title "The Little Schemer"
+                     :authors #{friedman, felleisen}})
+
+(def books [cities, wild-seed, embassytown, little-schemer])
+
+(defn has-author? [book author]
+  (if (contains? author (:authors book))
+    true
+    false))
+
+(has-author? cities china)             ;=> true
+(has-author? cities felleisen)         ;=> false
+(has-author? little-schemer felleisen) ;=> true
+(has-author? little-schemer friedman)  ;=> true
+(has-author? little-schemer octavia)   ;=> false
