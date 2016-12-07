ed3970c3afdce3b63aea127632a7cc2fc28e04f6
Travis Smith <travissmith94@hotmail.com>
2016-12-07 18:21:31 +0000

    alive? function

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -31,31 +31,38 @@
     (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (= (height rectangle) (width rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [px py] point]
+      (and (<= x1 px x2) 
+           (<= y1 py y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] inner]
+    (and (contains-point? outer (point x1 y1)) 
+         (contains-point? outer (point x2 y2)))))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
-(defn author-count [book]
-  :-)
+(defn author-count [book] 
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
-(defn add-author [book new-author]
-  :-)
+(defn add-author [book new-author] 
+  (let [title (:title book)
+        authors (:authors book)]
+      {:title title :authors (conj authors new-author)}))
 
 (defn alive? [author]
-  :-)
+  (not (contains? author :death_year)))
 
 (defn element-lengths [collection]
   :-)
