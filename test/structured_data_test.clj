3fea0ece96dc2074b545c7dd67ead68c88be9eef
Ilmo Raunio <ilmo.raunio@solita.fi>
2016-12-27 10:18:01 +0200

    Exercises plus some tests

--- test/structured_data_test.clj
+++ test/structured_data_test.clj
@@ -1,2 +1,12 @@
 (ns structured-data-test
-  (:use iloveponies.tests.structured-data))
+  (:use iloveponies.tests.structured-data
+        structured-data
+        midje.sweet))
+
+(facts "spiff-addendum"
+  (spiff [1 2]) => (throws Exception)
+  (spiff []) => (throws Exception))
+
+;; assignment specifies vector as input
+(facts "cutify-addendum"
+  (cutify '(1 2)) => (throws Exception))
