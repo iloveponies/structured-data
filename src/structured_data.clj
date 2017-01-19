c21c9962dfcb5237060a475dff627bdbd9601805
Jared Bagley <cabrill@gmail.com>
2017-01-19 02:59:59 +0200

    Completed exercises 1 through 9

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,33 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+    (Math/pow xx xx))
+)
 
 (defn spiff [v]
-  :-)
+  (cond
+    (empty? v) 0
+    (> 3 (count v)) (get v 0)
+    :else (+ (get v 0) (get v 2))
+  )
+)
+
 
 (defn cutify [v]
-  :-)
+  (conj v "<3")
+)
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[a b c d] v]
+    (cond
+      (= a nil) 0
+      (= c nil) a
+      :else (+ a c)
+    )
+  )
+)
+
 
 (defn point [x y]
   [x y])
@@ -19,22 +36,40 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (if (> x1 x2) (- x1 x2) (- x2 x1)))
+)
+
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (if (> y1 y2) (- y1 y2) (- y2 y1)))
+)
 
 (defn square? [rectangle]
-  :-)
+  (== (width rectangle) (height rectangle))
+)
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle))
+)
 
 (defn contains-point? [rectangle point]
-  :-)
+    (let [[[x1 y1] [x2 y2]] rectangle
+          [px py] point]
+      (and (>= px x1) (<= px x2)
+           (>= py y1) (<= py y2))
+      )
+)
 
 (defn contains-rectangle? [outer inner]
-  :-)
+    (let [[[x1 y1] [x2 y2]] outer
+          [[px1 py1] [px2 py2]] inner]
+      (and (contains-point? outer [px1 py1])
+           (contains-point? outer [px2 py2])
+      )
+  )
+)
 
 (defn title-length [book]
   :-)
