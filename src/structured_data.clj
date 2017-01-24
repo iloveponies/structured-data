de4a7225c4e844228d3fbc81245ac6c9b4ce495e
outsou <otto.hantula@hotmail.com>
2017-01-24 21:45:16 +0200

    dun

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -29,9 +29,7 @@
     (- y2 y1)))
 
 (defn square? [rectangle]
-  (if (== (width rectangle) (height rectangle))
-    true
-    false))
+  (== (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
   (* (width rectangle) (height rectangle)))
@@ -39,15 +37,11 @@
 (defn contains-point? [rectangle point]
   (let [[[x1 y1][x2 y2]] rectangle
         [xPoint yPoint] point]
-    (if (and (<= x1 xPoint x2) (<= y1 yPoint y2))
-      true
-      false)))
+    (and (<= x1 xPoint x2) (<= y1 yPoint y2))))
 
 (defn contains-rectangle? [outer inner]
   (let [[point1 point2] inner]
-    (if (and (contains-point? outer point1) (contains-point? outer point2))
-      true
-      false)))
+    (and (contains-point? outer point1) (contains-point? outer point2))))
 
 (defn title-length [book]
   (count (:title book)))
@@ -56,9 +50,7 @@
   (count (:authors book)))
 
 (defn multiple-authors? [book]
-  (if (> (author-count book) 1)
-    true
-    false))
+  (> (author-count book) 1))
 
 (defn add-author [book new-author]
   (let [authors (:authors book)
@@ -66,9 +58,7 @@
     (assoc book :authors new-authors)))
 
 (defn alive? [author]
-  (if (contains? author :death-year)
-    false
-    true))
+  (not (contains? author :death-year)))
 
 (defn element-lengths [collection]
   (map count collection))
@@ -81,56 +71,67 @@
   (map :title books))
 
 (defn monotonic? [a-seq]
-  (if (or (apply <= a-seq) (apply >= a-seq))
-    true
-    false))
+  (or (apply <= a-seq) (apply >= a-seq)))
 
 (defn stars [n]
   (apply str (repeat n \*)))
 
 (defn toggle [a-set elem]
-  :-)
+  (if (contains? a-set elem)
+    (disj a-set elem)
+    (conj a-set elem)))
 
 (defn contains-duplicates? [a-seq]
-  :-)
+  (< (count (set a-seq)) (count a-seq)))
 
 (defn old-book->new-book [book]
-  :-)
+    (let [authors (:authors book)]
+      (assoc book :authors (set authors))))
 
 (defn has-author? [book author]
-  :-)
+  (contains? (:authors book) author))
 
 (defn authors [books]
-  :-)
+  (apply clojure.set/union (map :authors books)))
 
 (defn all-author-names [books]
-  :-)
+  (set (map :name (authors books))))
 
 (defn author->string [author]
-  :-)
+  (let [name (:name author)
+        years (if (:birth-year author)
+                (str " (" (:birth-year author) " - " (:death-year author) \))
+                )]
+    (str name years)))
 
 (defn authors->string [authors]
-  :-)
+  (apply str (interpose ", " (map author->string authors))))
 
 (defn book->string [book]
-  :-)
+  (str (:title book) ", written by " (authors->string (:authors book))))
 
 (defn books->string [books]
-  :-)
+  (let [num-of-books (count books)
+        books-string (str (apply str (interpose ". " (map book->string books))) \.)]
+    (cond
+      (== num-of-books 0) "No books."
+      (== num-of-books 1) (str "1 book. " books-string)
+      :else (str num-of-books " books. " books-string)
+      )))
 
 (defn books-by-author [author books]
-  :-)
+  (filter (fn [book] (has-author? book author)) books))
 
 (defn author-by-name [name authors]
-  :-)
+  (first (filter (fn [author] (= (:name author) name)) authors)))
 
 (defn living-authors [authors]
-  :-)
+  (filter alive? authors))
 
 (defn has-a-living-author? [book]
-  :-)
+   (not (empty? (living-authors (:authors book)))))
 
 (defn books-by-living-authors [books]
-  :-)
+  (filter has-a-living-author? books))
 
 ; %________%
