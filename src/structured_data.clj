b4189c55c28132fa8ff0a30463c350e15b27f9ae
preeti1205 <preeti.bhandari1205@gmail.com>
2017-01-23 23:25:18 -0500

    structures

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,24 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+    (Math/pow xx xx)))
+
 
 (defn spiff [v]
-  :-)
+  (let [a (get v 0)
+        b (get v 2)]
+    (+ a b)))
+
+;(spiff [1 2])   NullPointerException in case of vector of size < 3
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z] v]
+    (+ x z)))
+
 
 (defn point [x y]
   [x y])
@@ -19,31 +27,68 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
+
+;i defined it here
+(defn abs [x]
+  (if (< x 0)
+    (* -1 x)
+    x))
 
 (defn square? [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (if (== (abs (- x2 x1)) (abs (- y2 y1)))
+      true
+      false)))
 
 (defn area [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (abs (* (- y2 y1) (- x2 x1)))))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [x y] point]
+    (if (and (<= x1 x x2) (<= y1 y y2))
+        true
+        false)))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x3 y3] [x4 y4]] inner]
+    (if (and (contains-point? outer [x3 y3])
+             (contains-point? outer [x4 y4]))
+      true
+      false)))
+
+;
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def friedman {:name "Daniel Friedman" :birth-year 1944})
+(def felleisen {:name "Matthias Felleisen"})
+
+(def cities {:title "The City and the City" :authors [china]})
+(def wild-seed {:title "Wild Seed", :authors [octavia]})
+(def embassytown {:title "Embassytown", :authors [china]})
+(def little-schemer {:title "The Little Schemer"
+                     :authors [friedman, felleisen]})
+;
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (if (> (author-count book) 1)
+    true
+    false))
 
 (defn add-author [book new-author]
   :-)
