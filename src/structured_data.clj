930ab0049e6348411e322b722cb7372da4df9c96
devgranlund <tuomas.granlund@gmail.com>
2017-04-16 10:45:34 +0300

    More tests are passing

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,18 +1,25 @@
 (ns structured-data)
 
+; Using let to give name for local value and call to Java Math.pow -method.
 (defn do-a-thing [x]
   (let [xx (+ x x)]
     (Math/pow xx xx))
   )
 
+; Takes a vector and returns the sum of it's first and third element.
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2))
+  )
 
+; Adds string at the end of the vector.
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
+; Destructuring vector.
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z] v]
+    (+ x z)
+    ))
 
 (defn point [x y]
   [x y])
@@ -21,22 +28,43 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)
+    )
+  )
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)
+    )
+  )
 
 (defn square? [rectangle]
-  :-)
+   (let [[[x1 y1] [x2 y2]] rectangle]
+    (== (- x1 x2) (- y1 y2))
+    )
+  )
 
 (defn area [rectangle]
-  :-)
+   (let [[[x1 y1] [x2 y2]] rectangle]
+    (* (- x2 x1) (- y2 y1))
+    )
+  )
 
+; Check if point is inside the rectangle
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [x3 y3] point]
+    (and (<= x1 x3 x2) (<= y1 y3 y2))
+    )
+  )
 
+; Check if inner rectangle is inside outer rectangle
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x3 y3] [x4 y4]] inner]
+    (and (contains-point? outer [x3 y3]) (contains-point? outer [x4 y4]))
+    )
+  )
 
 (defn title-length [book]
   :-)
