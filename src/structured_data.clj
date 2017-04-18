e61ff3da4f1248b81d4cf9bc0534e84a670ed68d
Miika Pärni <miika.parni@helsinki.fi>
2017-04-18 15:39:22 +0300

    Structured data 1

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,25 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let  [xx (+ x x)]
+     (float (reduce * (repeat xx xx)))
+    )
+)
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2))
+)
 
 (defn cutify [v]
-  :-)
+  (conj v "<3")
+)
+
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [n[ (get v 0) (get v 2)] ]
+  (+ (first n) (second n)))
+)
+
 
 (defn point [x y]
   [x y])
@@ -19,22 +28,52 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)
+  )
+)
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+   (- y2 y1)
+  )
+)
 
 (defn square? [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+  (cond (= (- y1 y2) (- x1 x2)) true
+    :else false))
+)
+
 
 (defn area [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+  (let [h (height rectangle)]
+  (let [w (width rectangle)]
+  (* w h))))
+)
 
 (defn contains-point? [rectangle point]
-  :-)
+   (let [[[x1 y1] [x2 y2]] rectangle]
+   (let [[zx1 zy1] point]
+   (cond
+     (and (and (>= zx1 x1 ) (<= zx1 (+ x1 (width rectangle) ))  (>= zy1 y1 ) (<= zy1 (+ y1 (height rectangle) )))) true
+    :else false)
+     )
+     )
+)
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] inner]
+    (let [p1 (point x1 y1)]
+    (let [p2 (point x2 y2)]
+    (cond
+      (and (contains-point? outer p1) (contains-point? outer p2)) true
+    :else false)
+    )))
+)
+
+
 
 (defn title-length [book]
   :-)
