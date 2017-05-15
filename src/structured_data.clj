25c83b69444695ad19fc3afa67ec6e78f8e0f488
retrazil <retrazil@gmail.com>
2017-05-15 23:30:16 +0530

    finish all exercises

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -127,9 +127,18 @@
   (set (map :name (authors books))))
 
 (def furaido {:name "Daniel Friedman" :birth-year 1944})
+(def chin {:name "China Miéville", :birth-year 1972})
 (def octa {:name "Octavia E. Butler"
-              :birth-year 1947
-              :death-year 2006})
+           :birth-year 1947
+           :death-year 2006})
+(def felle {:name "Matthias Felleisen"})
+
+(def embassy {:title "Embassytown", :authors #{chin}})
+(def city {:title "The City and the City" :authors #{chin}})
+(def wild {:title "Wild Seed", :authors #{octa}})
+(def schemer {:title "The Little Schemer"
+              :authors #{furaido, felle}})
+(def sakka #{chin, felle, octa, furaido})
 
 (defn author->string [author]
   (let [name (:name author)
@@ -142,27 +151,51 @@
      :else name)))
 
 (defn authors->string [authors]
-  :-)
+  (apply str (interpose ", " (map author->string authors))))
 
 (defn book->string [book]
-  :-)
+  (let [title (:title book)
+        authors (authors->string (:authors book))]
+    (str title ", written by " authors )))
 
 (defn books->string [books]
-  :-)
+  (let [count (count books)
+        book-list (interpose ". " (map book->string books)) ; add full stops between subsequent books
+        books-string (apply str book-list)] ; convert book-list to string
+      (cond 
+       (= count 0) "No books."
+       (= count 1) (str count " book. " books-string ".")
+       :else (str count " books. " books-string "."))))
 
 (defn books-by-author [author books]
-  :-)
+  (let [contains-author? (fn [book] (has-author? book author))]
+    (filter contains-author? books)))
 
 (defn author-by-name [name authors]
-  :-)
+  (let [has-name? (fn [x] (= (:name x) name))]
+    (first (filter has-name? authors))))
 
 (defn living-authors [authors]
-  :-)
+  (filter alive? authors))
+
+(def tolkien {:name "J. R. R. Tolkien" :birth-year 1892 :death-year 1973})
+(def chris {:name "Christopher Tolkien" :birth-year 1924})
+(def k {:name "Guy Gavriel Kay" :birth-year 1954})
+
+(def silmari {:title "Silmarillion"
+                   :authors #{tolkien, chris, k}})
+
+(def d {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
+(def zela {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
+
+(def deus {:title "Deus Irae", :authors #{d, zela}})
 
 (defn has-a-living-author? [book]
-  :-)
+  (let [death-years (map :death-year (:authors book))
+        living-authors (filter not death-years)] ; remove dead people
+    (if (empty? living-authors) false true)))
 
 (defn books-by-living-authors [books]
-  :-)
+  (filter has-a-living-author? books))
 
                                         ; %________%
