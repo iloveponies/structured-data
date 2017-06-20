a7d5e50dc0f1d75ccd45c29064b146fcebfaca37
Tiira Taneli J <ttiira@da5-cs-bk107-20.pc.helsinki.fi>
2017-06-20 17:37:53 +0300

    finished

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -144,21 +144,26 @@
          :else (str (count books)
                     " books. "
                     (apply str (interpose ". "
-                                    (map book->string books)))))))
+                                    (map book->string books)))
+                    "."))))
 
 (defn books-by-author [author books]
-  :-)
+  (filter (fn [book] (has-author? book author))
+          books))
 
 (defn author-by-name [name authors]
-  :-)
+  (first (filter (fn [author] (= name (:name author)))
+                 authors)))
 
 (defn living-authors [authors]
-  :-)
+  (filter (fn [author] (alive? author))
+          authors))
 
 (defn has-a-living-author? [book]
-  :-)
+  (not (empty? (living-authors (:authors book)))))
 
 (defn books-by-living-authors [books]
-  :-)
+  (filter (fn [book] (has-a-living-author? book))
+          books))
 
 ; %________%
