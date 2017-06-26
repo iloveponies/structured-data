a2c72b875e9ccf92acd7aaea298940f6b84bc45d
xbexbex <petja.valkama@gmail.com>
2017-06-26 14:39:31 +0300

    10

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,22 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+    (Math/pow xx xx)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z] v]
+    (+ x z)))
+
+(spiff-destructuring [1 2 3])
+
+(do-a-thing 1)
 
 (defn point [x y]
   [x y])
@@ -19,22 +25,37 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (- (get (get rectangle 1) 0) (get (get rectangle 0) 0) ))
+
+(width (rectangle [1 1] [4 1]))
+(height (rectangle [1 1] [4 5]))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (if (= (height rectangle) (width rectangle)) true false))
+
+(square? (rectangle [3 2] [1 1]))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [x3 y3] point]
+    (and (<= x1 x3 x2) (<= y1 y3 y2))))
+
+(contains-point? (rectangle [0 0] [2 2])
+                 (point 1 -2))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] inner]
+    (and (contains-point? outer (point x1 y1)) (contains-point? outer (point x2 y2)))))
+
+(contains-rectangle? (rectangle [0 0] [3 3])
+                     (rectangle [1 1] [4 2]))
 
 (defn title-length [book]
   :-)
