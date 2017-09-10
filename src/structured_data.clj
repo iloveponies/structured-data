026f35867f5176200cbd924a0b7dec6fb107abd3
rkelkka <raine_kelkka@hotmail.com>
2017-09-10 20:19:36 +0300

    Structured data: exercises 1 - 18

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,20 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [plus (+ x x)]
+    (Math/pow plus plus)))
 
 (defn spiff [v]
-  :-)
+  (let [fst (get v 0)
+        trd (get v 2)]
+    (+ fst trd)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z] v]
+    (+ x z)))
 
 (defn point [x y]
   [x y])
@@ -19,52 +23,66 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[blx bly] (get rectangle 0)
+        [trx try] (get rectangle 1)]
+    (- trx blx)))
+
+
+
+
 
 (defn height [rectangle]
-  :-)
+  (let [[blx bly] (get rectangle 0)
+        [trx try] (get rectangle 1)]
+    (- try bly)))
 
 (defn square? [rectangle]
-  :-)
+  (= (height rectangle) (width rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[blx bly] (get rectangle 0)
+        [trx try] (get rectangle 1)
+        [px py] point]
+    (and (<= blx px trx) (<= bly py try))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[p1 p2] inner]
+    (and (contains-point? outer p1) (contains-point? outer p2))))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (let [with_new_author (conj (:authors book) new-author)]
+    (assoc book :authors with_new_author)))
 
 (defn alive? [author]
-  :-)
+  (not (contains? author :death-year)))
 
 (defn element-lengths [collection]
-  :-)
+  (map count collection))
 
 (defn second-elements [collection]
-  :-)
+  (let [get_second (fn [x] (get x 1))]
+    (map get_second collection)))
 
 (defn titles [books]
-  :-)
+  (map :title books))
 
 (defn monotonic? [a-seq]
   :-)
 
 (defn stars [n]
-  :-)
+  (apply str (repeat n "*")))
 
 (defn toggle [a-set elem]
   :-)
