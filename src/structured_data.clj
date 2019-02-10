b84c31045634787cf1cfd633d3c3543b83d5d8be
rsquelbut <java4job@gmail.com>
2019-02-10 21:12:53 +0100

    en attendant de faire mieux

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,20 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [thing (+ x x)]
+    (Math/pow thing thing)))
 
 (defn spiff [v]
-  :-)
+  (let [first (get v 0)
+        second (get v 2)]
+    (+ first second)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[a ? b] v]
+    (+ a b)))
 
 (defn point [x y]
   [x y])
@@ -19,34 +23,48 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 ?] [x2 ?]] rectangle]
+    (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[? y1] [? y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (== (height rectangle) (width rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [a b] point
+        left-in (>= a x1)
+        right-in (<= a x2)
+        bottom-in (>= b y1)
+        top-in (<= b y2)]
+    (and left-in right-in bottom-in top-in)
+    ))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[bottom-left top-right] inner
+        bottom-left-in (contains-point? outer bottom-left)
+        top-right-in (contains-point? outer top-right)]
+    (and bottom-left-in top-right-in)
+    ))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
 (defn add-author [book new-author]
-  :-)
+  (let [new-authors (conj (:authors book) new-author)]
+    (assoc book :authors new-authors)))
 
 (defn alive? [author]
   :-)
